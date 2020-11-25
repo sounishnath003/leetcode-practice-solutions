e7842b754913f0844248bf142a973b1643ea5892
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2020-11-25 21:11:07 +0530

    36 ms

--- solutions/2. Add Two Numbers.cpp
+++ solutions/2. Add Two Numbers.cpp
@@ -10,7 +10,26 @@
  */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
-    }
-};
+    ListNode* addTwoNumbers(ListNode* root1, ListNode* root2) {
+        if(!root1) {
+            return root2;
+        }
+        if(!root2) {
+            return root1 ;
+        }
+        ListNode *answer = NULL, *t, *prev(NULL) ;
+        int cursum = 0, carry = 0 ;
+        while(root1 or root2) {
+            cursum = carry + ( root1 ? root1->val : 0 ) + ( root2 ? root2->val : 0 ) ;
+            carry = (cursum > 9) ? 1 : 0 ;
+            cursum %= 10 ;
+            t = new ListNode(cursum) ;
+            if(!answer) { answer = t ; }
+            else {
+                prev->next = t ;
+            }
+            prev = t ;
+            if(root1) { root1 = root1->next ; }
+            if(root2) { root2 = root2->next ; }
+        }
+        if(carry > 0) {
