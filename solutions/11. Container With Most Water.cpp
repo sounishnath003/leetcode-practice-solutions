0ee7300652d7cec6134bd179f2c9cf672093fda3
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2020-11-28 22:41:28 +0530

    36 ms

--- solutions/11. Container With Most Water.cpp
+++ solutions/11. Container With Most Water.cpp
@@ -4,6 +4,19 @@ public:
     const int inf = 0x3f3f3f3f;
     
     int maxArea(vector<int>& nums) {
+        
+        /*
+            O(N*N)solutions:
+            
+            for(i,0,n){
+                for(j,i+1,n){
+                    minn = min(height[i], height[j]);
+                    maxv = max(maxv, minn*(j-i));
+                }
+            }
+            return maxv;
+        */
+        
         int n = nums.size();
         int maxv = -1 * inf;
         int i = 0, j = n-1;
