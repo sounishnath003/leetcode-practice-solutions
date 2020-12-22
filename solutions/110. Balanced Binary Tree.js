3d288a6f3ed0ffdb9eeceaffdbede0141cac1562
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2020-12-22 19:10:00 +0530

    88 ms

--- solutions/110. Balanced Binary Tree.js
+++ solutions/110. Balanced Binary Tree.js
@@ -10,6 +10,30 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
+​
+/*
+    Solution 1 - O(N*N) looks
+    ---------------------------
+var isBalanced = function(root) {
+    if(!root) {
+        return true ;
+    }
+    
+    function dfs(root) {
+        if (!root) {
+            return 0 ;
+        }
+        return Math.max(dfs(root.left), dfs(root.right)) + 1;
+    }
+    
+    if(Math.abs(dfs(root.left) - dfs(root.right)) > 1) {
+        return false;
+    }
+    
+    return isBalanced(root.left) && isBalanced(root.right);
+};
+*/
+​
 var isBalanced = function(root) {
     if(!root) {
         return true ;
@@ -18,13 +42,16 @@ var isBalanced = function(root) {
     function dfs(root) {
         if (!root) {
             return 0 ;
-        }
-        return Math.max(dfs(root.left), dfs(root.right)) + 1;
+        }
+        
+        leftHeight = dfs(root.left);
+        rightHeight = dfs(root.right);
+        balanceFactor = Math.abs (leftHeight - rightHeight);
+        
+        if (balanceFactor > 1 || leftHeight == -1 || rightHeight == -1) {return -1;}
+        
+        return 1 + Math.max(leftHeight, rightHeight) ;
     }
     
-    if(Math.abs(dfs(root.left) - dfs(root.right)) > 1) {
-        return false;
-    }
-    
-    return isBalanced(root.left) && isBalanced(root.right);
+   return dfs(root) != -1;
 };
