ae0d5a12c478d30228ab747ab4a534269e392fff
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2021-01-18 10:21:34 +0530

    720 ms

--- solutions/1641. Count Sorted Vowel Strings.cpp
+++ solutions/1641. Count Sorted Vowel Strings.cpp
@@ -1,6 +1,9 @@
 class Solution {
     string vowels = "aeiou";
 public:
+    int easyMathSolution(int n) {
+        return ((n+4)*(n+3)*(n+2)*(n)) / 24
+    }
     int countVowelStrings(int n) {
         int ans = 0;
         for (char x : vowels ) {
@@ -11,7 +14,7 @@ public:
     int dfs(char lastvowel, int n) {
         if (n==0) {
             return 1;
-        }
+        }
         int ans=0;
         for (char vowel : vowels) {
             if (vowel >= lastvowel) 
