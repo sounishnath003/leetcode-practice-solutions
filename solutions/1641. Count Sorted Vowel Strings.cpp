182219b597aeeb50e3043b692a225cd6a458da45
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2021-01-18 10:16:43 +0530

    720 ms

---
+++ solutions/1641. Count Sorted Vowel Strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    string vowels = "aeiou";
+public:
+    int countVowelStrings(int n) {
+        int ans = 0;
+        for (char x : vowels ) {
+            ans += dfs(x, n-1);
+        }
+        return ans;
+    }
+    int dfs(char lastvowel, int n) {
+        if (n==0) {
+            return 1;
+        }
+        int ans=0;
+        for (char vowel : vowels) {
+            if (vowel >= lastvowel) 
+                ans+=dfs(vowel, n-1);
+        }
+        return ans;
+    }
+};
