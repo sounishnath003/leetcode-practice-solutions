b60b17a55f4050037dcdbe0bee303c5d4b0e0309
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2021-05-02 22:33:43 +0530

    N/A

--- solutions/630. Course Schedule III.java
+++ solutions/630. Course Schedule III.java
@@ -37,3 +37,23 @@ class Solution {
     Comparator<Course> comp = (a, b) -> a.lastDay - b.lastDay;
         PriorityQueue<Course> courses = new PriorityQueue<Course>(courseee.length + 1, comp);
         for(int[] c : courseee) {
+            courses.add(new Course(c[0], c[1]));
+        }
+        PriorityQueue<Integer> alreadyTaken = new PriorityQueue<>((a, b) -> (b-a));
+        int time = 0;
+        for(Course course : courses) {
+            if(time+course.duration <= course.lastDay) {
+                alreadyTaken.add(course.duration);
+                time += course.duration;
+            } else {
+                if (alreadyTaken.isEmpty()==false && alreadyTaken.peek() > course.duration) {
+                    int x = alreadyTaken.poll();
+    time -= x;
+                    time += course.duration;
+                    alreadyTaken.add(course.duration);
+                }
+            }
+        }
+        return alreadyTaken.size();
+    }
+}
