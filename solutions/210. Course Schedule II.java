8ccbf87c765faa888f1dfd6fb86ceeab02ad5897
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2021-05-03 11:41:07 +0530

    2 ms

--- solutions/210. Course Schedule II.java
+++ solutions/210. Course Schedule II.java
@@ -1,3 +1,29 @@
+class Solution {
+    List<Integer>[] graph;
+    boolean[] visited;
+    boolean[] explored;
+    Stack<Integer> stk;
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        graph = new ArrayList[numCourses];
+        for(int i = 0; i < numCourses; i++){
+            graph[i] = new ArrayList<>();
+        }
+        for(int i = 0; i < prerequisites.length; i++){
+            graph[prerequisites[i][1]].add(prerequisites[i][0]);
+        }
+        
+        explored = new boolean[numCourses];
+        visited = new boolean[numCourses];
+        
+        for(int i = 0; i < numCourses; i++){
+            if(!visited[i]){
+                if(isCyclic(i)){
+                    return new int[]{};
+                }
+            }
+        }
+        
+        visited = new boolean[numCourses];
         stk = new Stack<>();
         for(int i = 0; i < numCourses; i++){
             if(!visited[i]){
@@ -23,7 +49,7 @@
             }
             else if(!explored[j]){
                 return true;
-            }
+            }
         }
         explored[i] = true;
         return false;
