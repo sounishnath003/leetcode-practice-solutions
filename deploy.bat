426314b95026b2f2a10fde632014f913ceb97ce6
sounishnath003 <flock.sinasini@gmail.com>
2021-05-04 20:06:26 +0530

    .

--- deploy.bat
+++ deploy.bat
@@ -6,6 +6,7 @@ copy ..\solutions solutions
 del /f/q/s ..\solutions
 rmdir ..\solutions
 xcopy ..\solutions solutions
+go run main.go
 xcopy solutions leetcode-practice-app\src\data
 git add .
 git commit -am "Updated database on timestamp: " < date /t
