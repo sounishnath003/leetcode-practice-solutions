8aa03c199e8a9687e4391561d5973a834707f1e6
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2021-05-26 12:58:56 +0530

    5 ms

--- solutions/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.java
+++ solutions/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.java
@@ -1,5 +1,10 @@
 class Solution {
     public int minPartitions(String n) {
+        /*
+            1) If the input has multiple digits, then you can solve for each digit independently, and merge the answers to form numbers that add up to that input.
+            
+            2) Thus the answer is equal to the max digit.
+        */
         int ans = n.charAt(0) - '0';
         for (int i = 1; i < n.length(); i++) {
             int x = n.charAt(i) - '0';
