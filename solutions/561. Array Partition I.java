74572a08ccdda982eeaaeea98fca14a7532c8453
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2021-06-12 21:57:18 +0530

    17 ms

---
+++ solutions/561. Array Partition I.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int ans = 0;
+​
+        for (int i = 0; i < nums.length; i += 2) {
+            ans += nums[i]; // pair = { nums[i], nums[i+1] }
+        }
+​
+        return ans;
+    }
+}
