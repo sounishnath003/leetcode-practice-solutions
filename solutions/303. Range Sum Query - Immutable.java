f92827f9e1daa27c9002f012755b82bb2c161816
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2021-06-18 22:08:59 +0530

    9 ms

--- solutions/303. Range Sum Query - Immutable.java
+++ solutions/303. Range Sum Query - Immutable.java
@@ -1,3 +1,30 @@
+public class NumArray {
+​
+    class SegmentTreeNode {
+        private int start, end;
+        private  SegmentTreeNode left, right;
+        private  int sum;
+​
+        public SegmentTreeNode(int start, int end) {
+            this.start = start;
+            this.end = end;
+            this.left = null;
+            this.right = null;
+            this.sum = 0;
+        }
+    }
+​
+    SegmentTreeNode root = null;
+​
+    public NumArray(int[] nums) {
+        root = buildTree(nums, 0, nums.length-1);
+    }
+    // TC : O(n)
+    private SegmentTreeNode buildTree(int[] nums, int start, int end) {
+        if (start > end) {
+            return null;
+        } else {
+            SegmentTreeNode ret = new SegmentTreeNode(start, end);
             if (start == end) {
                 ret.sum = nums[start]; // leaf nodes
             } else {
@@ -27,7 +54,7 @@
                 updateHelper(root.right, pos, val);
             }
             root.sum = root.left.sum + root.right.sum;
-        }
+        }
     }
 ​
     public int sumRange(int i, int j) {
@@ -42,12 +69,3 @@
 ​
             int mid = root.start + (root.end - root.start) / 2; 
             if (end <= mid) {
-                return sumRangeHelper(root.left, start, end);
-            } else if (start >= mid+1) {
-                return sumRangeHelper(root.right, start, end);
-            }  else {
-                return  sumRangeHelper(root.left, start, mid) + sumRangeHelper(root.right, mid+1, end) ;
-            }
-        }
-    }
-}
