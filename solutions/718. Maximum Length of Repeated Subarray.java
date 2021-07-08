6540c82cfc4162ff81c00e08de0875b14e747eab
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2021-07-08 19:10:25 +0530

    51 ms

--- solutions/718. Maximum Length of Repeated Subarray.java
+++ solutions/718. Maximum Length of Repeated Subarray.java
@@ -1,35 +1,3 @@
-// Maximum Length of Repeated Subarray
-class Solution {
-    
-    public int findLength(int[] arr1, int[] arr2) {
-        final int n = arr1.length;
-        final int m = arr2.length;
-        
-        // easy trick will be treating the problem as a LCS problem.
-        // apply that same stragey to this.
-        
-        // you will be good to go. 
-        // Actually very easy
-        
-        int[][] dp = new int[n+1][m+1];
-        int maxLen = 0;
-        
-        for (int i = 1; i < dp.length; i++) {
-            for (int j = 1; j < dp[i].length; j++) {
-                int elem1 = arr1[i-1];
-                int elem2 = arr2[j-1];
-                
-                if (elem1 == elem2) {
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }
-                maxLen = Math.max( maxLen, dp[i][j]);
-            }
-        }
-        
-        return maxLen;
-    }
-    
-        public int findLength2(int[] arr1, int[] arr2) {
             HashMap<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();
 ​
             for (int i = 0; i < arr1.length; i++) {
