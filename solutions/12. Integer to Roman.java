e09cbe7ec78ce7acb7ebd8950c8a9d564204dcb9
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2021-07-10 08:32:34 +0530

    4 ms

---
+++ solutions/12. Integer to Roman.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String intToRoman(int num) {
+        String[] roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] value = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < roman.length; i++) {
+            while (num >= value[i]) {
+                sb.append(roman[i]);
+                num -= value[i];
+            }
+        }
+        return sb.toString();
+    }   
+}
