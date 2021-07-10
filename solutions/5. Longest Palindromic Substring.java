8655d269ba701acd303fb013488c1121c732d0a0
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2021-07-10 09:34:33 +0530

    Update 5. Longest Palindromic Substring.java

--- solutions/5. Longest Palindromic Substring.java
+++ solutions/5. Longest Palindromic Substring.java
@@ -31,3 +31,43 @@ class Solution {
         return s.substring(maxCenter, maxCenter + maxLen);
     }
 }
+
+// One Of The Easiest Implementation Solution
+// However need to fix some rare cases
+
+class Solution {
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        if (n == 0)
+            return "";
+
+        StringBuilder sb = new StringBuilder(s);
+        String t = sb.reverse().toString();
+
+        int[][] dp = new int[n + 1][n + 1];
+
+        int maxlen = 0;
+        int startIndex = -1;
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[i].length; j++) {
+                char x = s.charAt(i - 1);
+                char y = t.charAt(j - 1);
+
+                if (x == y) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    if (maxlen < dp[i][j]) {
+                        maxlen = dp[i][j];
+                        startIndex = i - dp[i][j];
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < dp.length; i++) {
+            System.out.println(Arrays.toString(dp[i]));
+        }
+
+        return s.substring(startIndex, startIndex + maxlen);
+    }
+}
