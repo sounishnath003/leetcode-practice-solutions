a2815ad044b04aad0db78bd6d3ab39f832bb478f
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2021-07-18 19:46:52 +0530

    1 ms

--- solutions/25. Reverse Nodes in k-Group.java
+++ solutions/25. Reverse Nodes in k-Group.java
@@ -1,53 +1,58 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-​
-/**
- * LinkedListt
- */
-class LinkedListt {
-​
-    ListNode head;
-    ListNode tail;
-    int size = 0;
-​
-    LinkedListt() {
-        this.head = null;
-        this.tail = null;
-        this.size = 0;
+            this.size--;
+            return data;
+        }
     }
+}
 ​
-    public void addFirst(int data) {
-        ListNode newNode = new ListNode(data);
+class Solution {
 ​
-        if (this.head == null && this.size == 0) {
-            this.head = newNode;
-            this.tail = newNode;
-        } else {
-            newNode.next = this.head;
-            this.head = newNode;
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || k < 2) {
+            return head;
+        }
+        LinkedListt list = new LinkedListt();
+        
+        // putting all the nodes in a linked list
+        while (head != null) {
+            list.addLast(head.val);
+            head = head.next;
         }
 ​
-        this.size++;
-    }
+        LinkedListt result = null; // stores final answer
 ​
-    public void addLast(int data) {
-        ListNode newNode = new ListNode(data);
+        while (list.size > 0) {
+            int size = list.size;
+            LinkedListt temp = new LinkedListt();
+            
+            if (size >= k) {
+                // remove first
+                // add first
+                // which will reverse the linkedlist
 ​
-        if (this.head == null && this.size == 0) {
-            this.head = newNode;
-            this.tail = newNode;
-        } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
+                for (int i = 0; i < k; i++) {
+                    int data = list.removeFirst();
+                    temp.addFirst(data);
+                }
+            } else {
+                for (int i = 0; i < size; i++) {
+                    int data = list.removeFirst();
+                    temp.addLast(data);
+                }
+            }
+​
+            if (result == null) {
+                result = temp;   
+            } else {
+                result.tail.next = temp.head;
+                result.tail = temp.tail;
+                result.size += temp.size;
+​
+            }
         }
-​
-        this.size++;
+    
+        head = result.head;
+        
+        return head;
     }
+}
+​
