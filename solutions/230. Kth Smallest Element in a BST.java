a3cff20287fbf0cef73ad99c53ea4cf4fb0ec95e
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2021-08-04 11:02:16 +0530

    0 ms

--- solutions/230. Kth Smallest Element in a BST.java
+++ solutions/230. Kth Smallest Element in a BST.java
@@ -14,7 +14,31 @@
  * }
  */
 class Solution {
-    /*
+    private int count = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        TreeNode node = kth(root, k);
+        return node.val;
+    }
+    
+    // with Constant Space -- O(1), TC = O(height)
+    private TreeNode kth(TreeNode root, int k) {
+        
+        if (root == null) return null;
+        
+        // faith
+        TreeNode rl = kth(root.left, k);
+        if (rl != null) return rl;
+        
+        this.count++;
+        if (count == k) return root;
+        
+        TreeNode rr = kth(root.right, k);
+        if (rr != null) return rr;
+        
+        return null;
+    }
+    
+     /*
         jekono BST te inorder traverse korlei to sorted order pawa jai.
         question ta meaning less, but practice concepts of BST
         
@@ -22,10 +46,10 @@ class Solution {
         simply nodes.get(k-1) korlei asha kori hye jabe!
     */
     private static ArrayList<Integer> nodes = new ArrayList<>();
-    public int kthSmallest(TreeNode root, int k) {
+    public int kthSmallest2(TreeNode root, int k) {
         performInOrderTraversal(root);
         return nodes.get(k-1); // 0 based indexing
-    }
+    }
     
     private static void performInOrderTraversal(TreeNode root) {
         if (root == null) return;
