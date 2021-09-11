b12138ace8ed861c35915cde7a5335ed04905c3e
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2021-09-11 11:37:03 +0530

    4 ms

---
+++ solutions/41. First Missing Positive.java
@@ -0,0 +1,31 @@
+class Solution {
+    // leetcode 41. https://leetcode.com/problems/first-missing-positive/
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        // step 1. find occurence of one and mark out of range element
+        boolean one = false;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == 1) one = true;
+​
+            if(nums[i] < 1 || n < nums[i]) {
+                nums[i] = 1;
+            }
+        }
+        if(one == false) return 1;
+        // step 2. mark element in array
+        for(int i = 0; i < n; i++) {
+            int val = Math.abs(nums[i]);
+            int indx = val - 1;
+            nums[indx] = -Math.abs(nums[indx]);
+        }
+​
+        // step 3. check first missing positive
+        for(int i = 0; i < n; i++) {
+            if(nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+​
+}
