3c1f49dce81db539d56581567b1968bbf1e00c61
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2022-02-13 21:26:24 +0530

    2 ms

---
+++ solutions/845. Longest Mountain in Array.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int longestMountain(int[] arr) {
+        int answer = 0;
+        int n = arr.length;
+​
+        // go from 1...n-2
+        // seems Time: O(N2)
+        for (int i = 1; i <= n-2; i++) {
+            /*
+             * find if arr[i] is peak
+             * how? --> arr[i] > arr[i] > arr[i+1]
+             */
+​
+            if (arr[i] > arr[i-1] && arr[i] > arr[i+1]) {
+                int j = i;
+                int size = 1;
+​
+                // left neighours
+                while (j > 0 && arr[j] > arr[j-1]) {
+                    size++;
+                    j--;
+                }
+​
+                // right neighbours
+                j = i;
+                while (j < n - 1 && arr[j] > arr[j+1]) {
+                    size++;
+                    j++;
+                }
+​
+                answer = Math.max(answer, size);
+            } else {
+                // i++;
+            }
+        }
+        
+        /**
+            optimization would be-
+            you already know by the right neighbours who are the peak values
+            so you can increment i pointer directly, 
+            not to create dependency upon j for rightward traversal
+        */
+​
+        return answer;
+    }
+}
