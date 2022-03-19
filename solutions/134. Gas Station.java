d35bde2aab0ecf16473228a04f947455c6258a90
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2022-03-19 13:47:29 +0530

    13 ms

--- solutions/134. Gas Station.java
+++ solutions/134. Gas Station.java
@@ -1,41 +1,54 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        /*
-         * find the possible starting points if Gas > cost then only porer station e
-         * jete parbo!
-         */
-​
-        final int n = gas.length;
-        List<Integer> possibleStartingPoints = getPossibleStation(gas, cost);
-        
-        for (int startPoint : possibleStartingPoints) {
-            int fuel = 0;
-            boolean status = true;
-            for (int point = startPoint; point < startPoint + n; point++) {
-                int station = point % n;
-                fuel += gas[station] - cost[station];
-                if (fuel < 0) {
-                    status = false;
-                    break;
-                }
-            }
-            if (status == true) return startPoint;
-        }
-        return -1;
+        return GasStation.startingStation(gas, cost);
     }
-    
-        private static List<Integer> getPossibleStation(int[] gas, int[] cost) {
-        // TODO Auto-generated method stub
-        List<Integer> indexes = new ArrayList<Integer>();
-​
-        for (int i = 0; i < gas.length; i++) {
-            int g = gas[i];
-            int c = cost[i];
-            if (g >= c)
-                indexes.add(i);
-        }
-​
-        return indexes;
-    }
-    
 }
+​
+​
+public class GasStation {
+    private static class Station {
+        int index;
+        int cost;
+        int gas;
+        public Station(int index, int cost, int gas) {
+            super();
+            this.index = index;
+            this.cost = cost;
+            this.gas = gas;
+        }
+        @Override
+        public String toString() {
+            return "Station [index=" + index + ", cost=" + cost + ", gas=" + gas + "]";
+        }
+        
+    }
+    public static int startingStation(int[] gases, int[] costs) {
+        int n = gases.length;
+        Station[] stations = new Station[n];
+        for (int i = 0 ; i < n; i++) {
+            stations[i] = new Station(i, costs[i], gases[i]);
+        }
+        
+        int shortage = 0;
+        int stationpoint = 0;
+        int totalShortage = 0;
+        
+        for (Station station : stations) {
+            shortage += (station.gas - station.cost);
+            if (shortage < 0) {
+                shortage = 0;
+                stationpoint = station.index + 1;
+            }
+            totalShortage += (station.gas - station.cost);
+        }
+        
+        if (totalShortage >= 0) {
+            // System.out.println("STP: " + stationpoint);
+            return stationpoint;
+        } else {
+            // System.out.println("STP: " + -1);
+            return -1;
+        }
+    }
+}
+​
