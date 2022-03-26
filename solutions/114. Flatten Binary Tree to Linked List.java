2bcd03fb5e020adc5df74d5fd30f393b121dcfc3
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2022-03-26 13:52:02 +0530

    1 ms

--- solutions/114. Flatten Binary Tree to Linked List.java
+++ solutions/114. Flatten Binary Tree to Linked List.java
@@ -14,22 +14,27 @@
  * }
  */
 class Solution {
-    public void flatten(TreeNode root) {
-        if (root == null || (root.left == null && root.right == null))
-            return;
-        
-        if(root.left != null) {
-            TreeNode temp = root.right;
-            root.right = root.left;
-            root.left = null;
-            
-            TreeNode cr = root.right;
-            while(cr.right != null) {
-                cr = cr.right;
-            }
-            cr.right = temp;
-        }
+    private boolean isleafnode(TreeNode root) {return (root.left == null && root.right == null) ; }
+    private boolean hasLeft(TreeNode root) {return (root.left != null) ; }
+    
+    public void flatten(TreeNode root) {
+        if (root == null || isleafnode(root)) {
+            return ;
+        }
         
-        flatten(root.right);
-    }
+        // self
+        if (hasLeft(root)) {
+            TreeNode rootright = root.right;
+            root.right = root.left;
+            root.left = null;
+            
+            TreeNode temp = root.right;
+            while (temp.right != null) temp = temp.right;
+            temp.right = rootright;
+        }
+        
+        // faith
+        flatten(root.right);
+        
+    }
 }
