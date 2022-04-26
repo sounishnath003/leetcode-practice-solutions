95d65002ab7870b9bf7431be232fe838af278ff0
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2022-04-26 07:37:28 +0530

    Update 206. Reverse Linked List.java

--- solutions/206. Reverse Linked List.java
+++ solutions/206. Reverse Linked List.java
@@ -1,25 +1,37 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-    public ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
-​
-        while (curr != null){
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        
-        return prev;
-    }
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        return iterativeReverse(head);
+    }
+    public ListNode iterativeReverse(ListNode node) {
+        ListNode previous = null;
+        ListNode current = node;
+        while (current != null) {
+            ListNode nextnode = current.next;
+            current.next = previous;
+            previous = current;
+            current = nextnode;
+        }
+        return previous;
+    }
+    public ListNode recursiveReverse(ListNode node) {
+        if (node.next==null)
+            return node;
+
+        ListNode newhead = recursiveReverse(node.next);
+        node.next.next = node;
+        node.next = null;
+
+        return newhead;
+    }
 }
