2a6e4077879c056b53e8c1ee0899b1052f68316d
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2022-05-21 15:00:02 +0530

    1 ms

--- solutions/61. Rotate List.java
+++ solutions/61. Rotate List.java
@@ -1,13 +1,3 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
         if (k==0||head==null||head.next==null) return head;
