5f5bf5a7aeef064f2327f4c623b93721a9580209
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2022-09-23 20:44:34 +0530

    3696 ms

---
+++ solutions/1680. Concatenation of Consecutive Binary Numbers.py
@@ -0,0 +1,7 @@
+class Solution:
+    def concatenatedBinary(self, n: int) -> int:
+        result = ""
+        for i in range(1,n+1):
+            result += bin(i)[2:]
+​
+        return int(result, 2) % ((10**9)+7)
