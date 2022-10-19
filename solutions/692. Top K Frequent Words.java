68f1d149a70b3fe0acd49d1e10a94ac7d57ef0ef
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2022-10-19 21:09:24 +0530

    18 ms

--- solutions/692. Top K Frequent Words.java
+++ solutions/692. Top K Frequent Words.java
@@ -1,30 +1,39 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
-        
-        List<String> result = new LinkedList<>();
-        Map<String, Integer> map = new HashMap<>();
-        for(int i=0; i<words.length; i++)
-        {
-            if(map.containsKey(words[i]))
-                map.put(words[i], map.get(words[i])+1);
-            else
-                map.put(words[i], 1);
+        List<String> result = new ArrayList<>();
+        Map<String, Integer> frequencyMap = new HashMap<>();
+        for (String word : words) {
+            frequencyMap.put(word, frequencyMap.getOrDefault(word, 0) + 1);
         }
-        
-        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>(
-                 (a,b) -> a.getValue()==b.getValue() ? b.getKey().compareTo(a.getKey()) : a.getValue()-b.getValue()
-        );
-        
-        for(Map.Entry<String, Integer> entry: map.entrySet())
-        {
-            pq.offer(entry);
-            if(pq.size()>k)
-                pq.poll();
+        WordsQueue wordsQueue = new WordsQueue();
+        for (Map.Entry<String, Integer> kvPair : frequencyMap.entrySet()) {
+            wordsQueue.add(kvPair);
+            // normal heap logic for top = `K` results
+            if (wordsQueue.size() > k) {
+                wordsQueue.poll();
+            }
         }
 ​
-        while(!pq.isEmpty())
-            result.add(0, pq.poll().getKey());
-        
+        while (!wordsQueue.isEmpty()) {
+            Map.Entry<String, Integer> polled = wordsQueue.poll();
+            result.add(polled.getKey());
+        }
+        // because result so far has ASC order in
+        Collections.reverse(result);
+​
         return result;
+    }
+​
+    private class WordsQueue extends PriorityQueue<Map.Entry<String, Integer>> {
+        public WordsQueue() {
+            // min heap implication --
+            super((a, b) -> {
+                if (a.getValue() == b.getValue()) {
+                    return b.getKey().compareTo(a.getKey());
+                }
+​
+                return a.getValue() - b.getValue();
+            });
+        }
     }
 }
