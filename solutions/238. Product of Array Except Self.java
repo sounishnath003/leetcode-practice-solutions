ffc3fe66bf75e77d8b57ae89b8753a24e3cc83b9
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2022-10-30 01:26:51 +0530

    11 ms

--- solutions/238. Product of Array Except Self.java
+++ solutions/238. Product of Array Except Self.java
@@ -1,32 +1,36 @@
 class Solution {
-​
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
 ​
-        int[] leftProd = new int[n];
-        int[] rightProd = new int[n];
-​
-        // left
-        leftProd[0] = nums[0];
-        for(int i = 1; i < n; i++) {
-            leftProd[i] = leftProd[i - 1] * nums[i];
+        int[] leftwall = new int[n];
+        leftwall[0]=nums[0];
+        for (int i = 1; i < n; i++) {
+            leftwall[i] = leftwall[i-1] * nums[i];
         }
 ​
-        // right
-        rightProd[n - 1] = nums[n - 1];
-        for(int i = n - 2; i >= 0; i--) {
-            rightProd[i] = rightProd[i + 1] * nums[i];
+        int[] rightwall = new int[n];
+        rightwall[n-1]=nums[n-1];
+        for (int i = n-2; i >= 0; i--) {
+            rightwall[i] = rightwall[i+1] * nums[i];
+        }
+​
+        // System.out.println("leftwall = " + Arrays.toString(leftwall));
+        // System.out.println("rightwall = " + Arrays.toString(rightwall));
+​
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (i==0) {
+                result[i] = rightwall[i+1];
+            } else if (i == n-1) {
+                result[i] = leftwall[i-1];
+            } else {
+                int leftp = leftwall[i-1];
+                int rightp = rightwall[i+1];
+​
+                result[i] = leftp * rightp;
+            }
         }
 ​
-        // res
-        int[] res = new int[n];
-        res[0] = rightProd[1];
-        res[n - 1] = leftProd[n - 2];
-​
-        for(int i = 1; i < n - 1; i++) {
-            res[i] = leftProd[i - 1] * rightProd[i + 1];
-        }
-        return res;
+        return result;
     }
-​
 }
