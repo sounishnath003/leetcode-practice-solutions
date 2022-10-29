f5dbbd2c4bc61027540d5b066090bbb42b635a64
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2022-10-30 01:27:49 +0530

    11 ms

--- solutions/238. Product of Array Except Self.java
+++ solutions/238. Product of Array Except Self.java
@@ -12,7 +12,7 @@ class Solution {
         rightwall[n-1]=nums[n-1];
         for (int i = n-2; i >= 0; i--) {
             rightwall[i] = rightwall[i+1] * nums[i];
-        }
+        }
 ​
         // System.out.println("leftwall = " + Arrays.toString(leftwall));
         // System.out.println("rightwall = " + Arrays.toString(rightwall));
