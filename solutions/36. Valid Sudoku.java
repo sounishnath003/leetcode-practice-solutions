12d0f0669f438f7e0bc4e8a41907b71d480721a5
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2022-12-01 06:51:27 +0530

    30 ms

---
+++ solutions/36. Valid Sudoku.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> set = new HashSet<>();
+        for (int row = 0; row < 9; row++) {
+            for (int column = 0; column < 9; column++) {
+                char number = board[row][column];
+                if (number != '.') {
+                    if (!set.add(number + " in row " + row) ||
+                            !set.add(number + " in column " + column) ||
+                            !set.add(number + " in block " + (row / 3) + "," + (column / 3)))
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+}
