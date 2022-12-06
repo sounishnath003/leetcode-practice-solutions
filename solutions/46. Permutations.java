09c68ed7dd51e5d5cce9f8ac66f59a0d3d20fe44
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2022-12-06 20:59:02 +0530

    1 ms

--- solutions/46. Permutations.java
+++ solutions/46. Permutations.java
@@ -1,32 +1,37 @@
 class Solution {
-    /*
-        Question is fairly very Simple!
-        just traverse array and swap (i, curIndex) and backtrack
-    */
-    private List<List<Integer>> ans = new ArrayList<>(); 
     public List<List<Integer>> permute(int[] nums) {
-        f(nums, 0);
-        return ans;
+        List<List<Integer>> permutations = new ArrayList<>();
+        permute(nums, permutations, 0);
+        return permutations;
     }
-    private void f(int[] nums, int curIndex) {
-        if(curIndex == nums.length - 1){
-            List<Integer> list = Arrays.stream(nums).boxed().collect(Collectors.toList());
-            ans.add(list);
+​
+    private void permute(int[] nums, List<List<Integer>> permutations, int currentIndex) {
+        // TODO Auto-generated method stub
+        if (nums.length == currentIndex) {
+            permutations.add(toList(nums));
             return;
         }
-        
-        for(int i = curIndex; i < nums.length; i++) {
-            {
-                int t = nums[curIndex];
-                nums[curIndex] = nums[i];
-                nums[i] = t;
-            }
-            f(nums, curIndex+1);
-            {
-                int t = nums[curIndex];
-                nums[curIndex] = nums[i];
-                nums[i] = t;
-            }
-        }
+​
+        for (int i = currentIndex; i < nums.length; i++) {
+            swap(nums, currentIndex, i);
+            permute(nums, permutations, currentIndex + 1);
+            swap(nums, currentIndex, i);
+        }
+    }
+​
+    private List<Integer> toList(int[] nums) {
+        // TODO Auto-generated method stub
+        List<Integer> list = new ArrayList<>();
+        for (int x : nums)
+            list.add(x);
+        return list;
+    }
+​
+    private void swap(int[] nums, int currentIndex, int i) {
+        // TODO Auto-generated method stub
+        int t = nums[currentIndex];
+        nums[currentIndex] = nums[i];
+        nums[i] = t;
     }
 }
+​
