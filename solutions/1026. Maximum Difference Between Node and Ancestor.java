a8d4a3af51f61cbf8dcc014d1c1d3e49138885a3
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2022-12-09 21:57:06 +0530

    1 ms

--- solutions/1026. Maximum Difference Between Node and Ancestor.java
+++ solutions/1026. Maximum Difference Between Node and Ancestor.java
@@ -15,13 +15,31 @@
  */
 class Solution {
     public int maxAncestorDiff(TreeNode root) {
+        return _solver(root, root.val, root.val);
+    }
+​
+    private int _solver(TreeNode root, int low, int high) {
+        // TODO Auto-generated method stub
+        if (root == null) {
+            return Math.abs(high - low);
+        }
+​
+        low = Math.min(low, root.val);
+        high = Math.max(high, root.val);
+        int a1 = _solver(root.left, low, high);
+        int a2 = _solver(root.right, low, high);
+​
+        return Math.max(a1, a2);
+    }
+​
+    public int maxAncestorDiff2(TreeNode root) {
         if (root == null)
             return 0;
 ​
         int maxdiff1 = helper(root.left, root.val);
         int maxdiff2 = helper(root.right, root.val);
-        
-        int ma = Math.max(maxAncestorDiff(root.left), maxAncestorDiff(root.right));
+​
+        int ma = Math.max(maxAncestorDiff2(root.left), maxAncestorDiff2(root.right));
 ​
         return Math.max(ma, Math.max(maxdiff1, maxdiff2));
     }
@@ -35,6 +53,6 @@ class Solution {
         int diff2 = helper(root.right, a);
         int mydiff = Math.abs(root.val - a);
         return Math.max(mydiff, Math.max(diff1, diff2));
-    }
+    }
 }
 ​
