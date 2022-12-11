47a787f53bb8eeb58084134a13aa3f0cb346c57d
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2022-12-11 10:00:15 +0530

    1 ms

--- solutions/124. Binary Tree Maximum Path Sum.java
+++ solutions/124. Binary Tree Maximum Path Sum.java
@@ -1,43 +1,21 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-​
 class Solution {
-    private int maxSum = Integer.MIN_VALUE;
+    int sm = Integer.MIN_VALUE;;
 ​
     public int maxPathSum(TreeNode root) {
-        if (root == null)
-            return 0;
-​
-        _maxPathSum(root);
-        return this.maxSum;
+        solver(root);
+        return sm;
     }
 ​
-    private int _maxPathSum(TreeNode root) {
-​
-        if (root == null)
+    private int solver(TreeNode root) {
+        if (root == null) {
             return 0;
-        
-        // faith
-        int leftSum = Math.max(0, this._maxPathSum(root.left));
-        int rightSum = Math.max(0, this._maxPathSum(root.right));
+        }
 ​
-        int mysum = root.val + leftSum + rightSum;
+        int leftsum = Math.max(0,solver(root.left));
+        int rightsum = Math.max(0,solver(root.right));
 ​
-        this.maxSum = Math.max(this.maxSum, mysum);
-​
-        return Math.max(leftSum, rightSum) + root.val;
+        int mypathsum = leftsum + rightsum + root.val;
+        this.sm = Math.max(this.sm, mypathsum);
+        return Math.max(leftsum, rightsum) + root.val;
     }
 }
