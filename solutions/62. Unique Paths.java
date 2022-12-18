577a56d2ce4bcdfdc51ee692b0cc96315ff0c2aa
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2022-12-18 20:38:16 +0530

    0 ms

--- solutions/62. Unique Paths.java
+++ solutions/62. Unique Paths.java
@@ -1,7 +1,18 @@
 class Solution {
     private Map<String, Integer> dp = new HashMap<>();
     public int uniquePaths(int m, int n) {
-        return find_paths(0,0,m,n);
+        // return find_paths(0,0,m,n);
+        int[][] dp = new int[m+1][n+1];
+​
+        for (int col=0; col < n; col++) dp[0][col]=1;
+        for (int row=0; row<m; row++) dp[row][0]=1;
+​
+        for (int row=1; row < m; row++) {
+            for (int col=1; col < n; col++) {
+                dp[row][col] += dp[row-1][col] + dp[row][col-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 ​
     private int find_paths(int i, int j, int m, int n) {
@@ -9,7 +20,7 @@ class Solution {
         if (dp.containsKey("" + i + '@' + j)) return dp.get(""+i+'@'+j);
         if (i==m-1 && j==n-1) {
             return 1;
-        }
+        }
         int go_down=find_paths(i+1, j, m, n);
         int go_right=find_paths(i, j+1, m, n);
         dp.put(""+i+'@'+j, go_down+go_right);
