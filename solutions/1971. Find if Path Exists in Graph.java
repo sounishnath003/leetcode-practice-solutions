6609bb99552c1413f7f9c1865728e366a80ec0ad
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2022-12-19 20:25:06 +0530

    Update 1971. Find if Path Exists in Graph.java

--- solutions/1971. Find if Path Exists in Graph.java
+++ solutions/1971. Find if Path Exists in Graph.java
@@ -1,99 +1,99 @@
-	class Solution {
-		private static class UnionFind {
-			private int[] parents;
-			private int vertices;
+class Solution {
+	private static class UnionFind {
+		private int[] parents;
+		private int vertices;
 
-			public UnionFind(int vertices) {
-				super();
-				this.vertices = vertices;
-				this.parents = new int[vertices + 1];
-				for (int i = 0; i < vertices; i++)
-					parents[i] = i;
-			}
+		public UnionFind(int vertices) {
+			super();
+			this.vertices = vertices;
+			this.parents = new int[vertices + 1];
+			for (int i = 0; i < vertices; i++)
+				parents[i] = i;
+		}
 
-			@Override
-			public String toString() {
-				return "UnionFind [parents=" + Arrays.toString(parents) + ", vertices=" + vertices + "]";
-			}
+		@Override
+		public String toString() {
+			return "UnionFind [parents=" + Arrays.toString(parents) + ", vertices=" + vertices + "]";
+		}
 
-			public void union(int u, int v) {
-				if (u != v) {
-					int pa = find(u);
-					int pb = find(v);
-					parents[pb] = pa;
-				}
-			}
-
-			private int find(int u) {
-				// TODO Auto-generated method stub
-				int x = u;
-				while (x != parents[x]) {
-					x = parents[x];
-				}
-				parents[u] = x;
-				return x;
-			}
-
-			public boolean areConnected(int u, int v) {
-				return find(u) == find(v);
+		public void union(int u, int v) {
+			if (u != v) {
+				int pa = find(u);
+				int pb = find(v);
+				parents[pb] = pa;
 			}
 		}
 
-		public boolean validPath(int n, int[][] edges, int source, int destination) {
-//			return DFSTraversalMethod(n, edges, source, destination);
-			UnionFind uf = new UnionFind(n);
-			for (int[] edge : edges) {
-				int u = edge[0];
-				int v = edge[1];
-				uf.union(u, v);
+		private int find(int u) {
+			// TODO Auto-generated method stub
+			int x = u;
+			while (x != parents[x]) {
+				x = parents[x];
 			}
-			return uf.areConnected(source, destination);
+			parents[u] = x;
+			return x;
 		}
 
-		private boolean DFSTraversalMethod(int n, int[][] edges, int source, int destination) {
-			try {
-				Map<Integer, List<Integer>> graph = buildGraph(edges);
-
-				boolean[] visitedVertices = new boolean[n];
-				Stack<Integer> stack = new Stack<>();
-
-				markAsVisited(source, visitedVertices, stack);
-				while (!stack.isEmpty()) {
-					int node = stack.pop();
-					for (int neighbour : graph.get(node)) {
-						if (neighbour == destination)
-							return true;
-						if (!visitedVertices[neighbour]) {
-							markAsVisited(neighbour, visitedVertices, stack);
-						}
-					}
-				}
-
-				return false;
-
-			} catch (Exception e) {
-				// TODO: handle exception
-				return false;
-			}
-		}
-
-		private Map<Integer, List<Integer>> buildGraph(int[][] edges) {
-			Map<Integer, List<Integer>> graph = new HashMap<>();
-			for (int[] edge : edges) {
-				int u = edge[0];
-				int v = edge[1];
-
-				graph.putIfAbsent(u, new ArrayList<>());
-				graph.putIfAbsent(v, new ArrayList<>());
-
-				graph.get(u).add(v);
-				graph.get(v).add(u);
-			}
-			return graph;
-		}
-
-		private void markAsVisited(int source, boolean[] visitedVertices, Stack<Integer> stack) {
-			visitedVertices[source] = true;
-			stack.add(source);
+		public boolean areConnected(int u, int v) {
+			return find(u) == find(v);
 		}
 	}
+
+	public boolean validPath(int n, int[][] edges, int source, int destination) {
+//			return DFSTraversalMethod(n, edges, source, destination);
+		UnionFind uf = new UnionFind(n);
+		for (int[] edge : edges) {
+			int u = edge[0];
+			int v = edge[1];
+			uf.union(u, v);
+		}
+		return uf.areConnected(source, destination);
+	}
+
+	private boolean DFSTraversalMethod(int n, int[][] edges, int source, int destination) {
+		try {
+			Map<Integer, List<Integer>> graph = buildGraph(edges);
+
+			boolean[] visitedVertices = new boolean[n];
+			Stack<Integer> stack = new Stack<>();
+
+			markAsVisited(source, visitedVertices, stack);
+			while (!stack.isEmpty()) {
+				int node = stack.pop();
+				for (int neighbour : graph.get(node)) {
+					if (neighbour == destination)
+						return true;
+					if (!visitedVertices[neighbour]) {
+						markAsVisited(neighbour, visitedVertices, stack);
+					}
+				}
+			}
+
+			return false;
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			return false;
+		}
+	}
+
+	private Map<Integer, List<Integer>> buildGraph(int[][] edges) {
+		Map<Integer, List<Integer>> graph = new HashMap<>();
+		for (int[] edge : edges) {
+			int u = edge[0];
+			int v = edge[1];
+
+			graph.putIfAbsent(u, new ArrayList<>());
+			graph.putIfAbsent(v, new ArrayList<>());
+
+			graph.get(u).add(v);
+			graph.get(v).add(u);
+		}
+		return graph;
+	}
+
+	private void markAsVisited(int source, boolean[] visitedVertices, Stack<Integer> stack) {
+		visitedVertices[source] = true;
+		stack.add(source);
+	}
+}
