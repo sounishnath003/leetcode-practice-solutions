b16ced80f804b93d9dbaef40289ba5d86e35f40a
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2022-12-27 22:04:11 +0530

    Update 2279. Maximum Bags With Full Capacity of Rocks.java

--- solutions/2279. Maximum Bags With Full Capacity of Rocks.java
+++ solutions/2279. Maximum Bags With Full Capacity of Rocks.java
@@ -1,31 +1,68 @@
-class Solution {
-    public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
-        /*
-         * 1. try to fill the maximum rocks with the capacity 2. means sort the array by
-         * the capacity and present rocks 3. try to fill the rocks with additional rocks
-         * till its 0
-         */
-        int[] remainingSpace = new int[capacity.length];
-        for(int i =0; i<capacity.length;i++){
-               remainingSpace[i] = capacity[i] - rocks[i];
-        }
-​
-        Arrays.sort(remainingSpace);
-        int ans = 0;
-        for(int i =0;i<capacity.length;i++){
-            if(remainingSpace[i] == 0){
-                ans++;
-            }else{
-              if(additionalRocks>=remainingSpace[i]){
-                additionalRocks = additionalRocks - remainingSpace[i];
-                ans++;
-               }else{
-                   break;
-               }
-            }
-        }
-        return ans;
-    }
-​
+package src;
+
+import java.util.Arrays;
+
+// 2279. https://leetcode.com/problems/maximum-bags-with-full-capacity-of-rocks/
+public class MaximumBagsWithCapacity2279 {
+	public static void main(String[] args) {
+		int[] capacity = new int[] {91,54,63,99,24,45,78};
+		int[] rocks = new int[] { 35,32,45,98,6,1,25 };
+		int additionalRocks = 17;
+
+		System.out.println(new Solution().maximumBags(capacity, rocks, additionalRocks));
+	}
+
+	static class Solution {
+		private class RockBag implements Comparable<RockBag> {
+			int capacity;
+			int presentRocks;
+			int extraRequired;
+
+			public RockBag(int capacity, int presentRocks) {
+				super();
+				this.capacity = capacity;
+				this.presentRocks = presentRocks;
+				this.extraRequired = (capacity - presentRocks);
+			}
+
+			@Override
+			public String toString() {
+				return "RockBag [capacity=" + capacity + ", presentRocks=" + presentRocks + ", extraRequired="
+						+ extraRequired + "]";
+			}
+
+			@Override
+			public int compareTo(RockBag o) {
+				// TODO Auto-generated method stub
+				// descending order sorting
+				return this.extraRequired - o.extraRequired;
+			}
+		}
+
+		public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
+			/*
+			 * 1. try to fill the maximum rocks with the capacity 2. means sort the array by
+			 * the capacity and present rocks 3. try to fill the rocks with additional rocks
+			 * till its 0
+			 */
+			RockBag[] bags = getRockBags(capacity, rocks);
+			Printer.log(Arrays.toString(bags));
+			Arrays.sort(bags);
+			Printer.log(Arrays.toString(bags));
+
+			int target = 0;
+			for (int i = 0; i < bags.length && additionalRocks > 0; i++) {
+				additionalRocks = additionalRocks - bags[i].extraRequired;
+				if (additionalRocks>0) target++;
+			}
+			return target;
+		}
+
+		private RockBag[] getRockBags(int[] capacity, int[] rocks) {
+			RockBag[] bags = new RockBag[capacity.length];
+			for (int i = 0; i < bags.length; i++)
+				bags[i] = new RockBag(capacity[i], rocks[i]);
+			return bags;
+		}
+	}
 }
-​
