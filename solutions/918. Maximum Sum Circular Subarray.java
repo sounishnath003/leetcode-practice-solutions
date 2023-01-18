5e1065d1dcbfc480e21b42bcbf1c04aba51dff31
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2023-01-18 19:02:50 +0530

    4 ms

---
+++ solutions/918. Maximum Sum Circular Subarray.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxSubarraySumCircular(int[] nums) {
+        final int n = nums.length;
+        final int[] rightMax = new int[n];
+        rightMax[n - 1] = nums[n - 1];
+        for (int suffixSum = nums[n - 1], i = n - 2; i >= 0; --i) {
+            suffixSum += nums[i];
+            rightMax[i] = Math.max(rightMax[i + 1], suffixSum);
+        }
+        int maxSum = nums[0];
+        int specialSum = nums[0];
+        for (int i = 0, prefixSum = 0, curMax = 0; i < n; ++i) {
+            curMax = Math.max(curMax, 0) + nums[i];
+            // This is Kadane's algorithm.
+            maxSum = Math.max(maxSum, curMax);
+            prefixSum += nums[i];
+            if (i + 1 < n) {
+                specialSum = Math.max(specialSum, prefixSum + rightMax[i + 1]);
+            }
+        }
+        return Math.max(maxSum, specialSum);  
+    }
+}
