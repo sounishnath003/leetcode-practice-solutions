9bf4687fd483ad804ccb48650e38906b12cc80a8
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2024-08-25 08:56:49 +0530

    2 ms

---
+++ solutions/2341. Maximum Number of Pairs in Array.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+​
+class Solution {
+    public int[] numberOfPairs(int[] nums) {
+        int totalPairs = 0;
+        int leftOverElems = 0;
+​
+        Map<Integer, Integer> map = new HashMap<>();
+        // store the elems into map
+        for (int elem : nums) {
+            map.put(elem, map.getOrDefault(elem, 0) + 1);
+        }
+​
+        // iterate on map
+        for (Map.Entry<Integer, Integer> kv : map.entrySet()) {
+            int occurency = kv.getValue();
+​
+            // as pair == 2 elements.
+            // we can do occurency % 2
+            // // totalpairs will be the vagfol
+            // leftover will be the reminder
+​
+            totalPairs += occurency / 2;
+            leftOverElems += occurency % 2;
+        }
+​
+        return new int[] { totalPairs, leftOverElems };
+    }
+​
+}
