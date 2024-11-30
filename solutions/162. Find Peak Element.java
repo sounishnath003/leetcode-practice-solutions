6400bee683d78beaf646c7a520c36b55ce97818e
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2024-11-30 21:40:54 +0530

    0 ms

--- solutions/162. Find Peak Element.java
+++ solutions/162. Find Peak Element.java
@@ -1,19 +1,39 @@
 class Solution {
     public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        int peak = Integer.MIN_VALUE;
         
-        // Quick implementation of BinarySearch,
-        // as it needs to be solved in O(LogN)
-        
-        int lo = 0;
-        int hi = nums.length - 1;
-        while (lo < hi) {
-            int mid = (lo + hi) / 2;
-            if (nums[mid] < nums[mid + 1]) {
-                lo = mid + 1;
+        if (n==1) return 0;
+​
+        // check the 0th index
+        if (nums[0] > nums[1]) {
+            peak = nums[0];
+            return 0;
+        }
+​
+        // check the nth index
+        if (n - 2 >= 0 && nums[n - 1] > nums[n - 2]) {
+            peak = nums[n - 1];
+            return n-1;
+        }
+​
+        // time to confirm the intuition
+        int low = 1;
+        int high = n - 2;
+​
+        while (low <= high) {
+            int mid = (low + (high - low) / 2);
+            // is peak
+            if (nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1]) {
+                peak = mid;
+                return peak;
+            } else if (nums[mid] > nums[mid-1]) {
+                low = mid+1;
             } else {
-                hi = mid;
+                high=mid-1;
             }
         }
-        return lo;
+​
+        return -1;
     }
 }
