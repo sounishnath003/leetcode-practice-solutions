ac0328fff668a9d6d79d313317162597fa95aa5d
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2024-12-02 00:18:39 +0530

    59 ms

--- solutions/84. Largest Rectangle in Histogram.java
+++ solutions/84. Largest Rectangle in Histogram.java
@@ -1,48 +1,59 @@
-        final int n = heights.length;
-        int[] walls = new int[n];
+class Solution {
+    // https://leetcode.com/problems/largest-rectangle-in-histogram/
+    // :::IMPORTANT QUESTION:::
+    public int largestRectangleArea(int[] heights) {
+        // TC: O(N)
+        // SC: O(N)
 ​
-        Stack<Integer> stack = new Stack<>();
+        int n = heights.length;
+        int gmaxAns = 0;
 ​
-        for (int i = n - 1; i >= 0; i--) {
-            int curHeight = heights[i];
+        // stack
+        Stack<Integer> stack = new Stack<>();
 ​
-            while (stack.isEmpty() == false && heights[stack.peek()] >= curHeight)
-                stack.pop();
+        for (int i = 0; i < n; i++) {
+            // remove all the biggest elements from the stack than the current height[i]
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                int element = heights[stack.pop()];
+                int nse = i;
+                int pse = stack.isEmpty() ? -1 : stack.peek();
 ​
-            if (stack.isEmpty() == true)
-                walls[i] = n;
-            else
-                walls[i] = stack.peek();
+                int width = nse - pse - 1;
+                int height = element;
 ​
-            stack.push(i);
-        }
+                int area = width * height;
+                gmaxAns = Math.max(gmaxAns, area);
+            }
 ​
-        return walls;
-    }
+            stack.add(i); // add index not the value
+        }
 ​
-    private int[] getLeftHeightIndcies(int[] heights) {
-        // TODO Auto-generated method stub
+        // to process the left over elems
+        while (!stack.isEmpty()) {
+            int nse = n;
+            int element = stack.isEmpty() ? -1 : heights[stack.pop()];
+            int pse = stack.isEmpty() ? -1 : stack.peek();
+            gmaxAns = Math.max(gmaxAns, element * (nse - pse - 1));
+        }
 ​
-        final int n = heights.length;
-        int[] walls = new int[n];
+        return gmaxAns;
+    }
 ​
-        Stack<Integer> stack = new Stack<>();
+    public int largestRectangleArea2(int[] heights) {
+        // TC: O(N + N)
+        // SC: O(2N)
 ​
-        for (int i = 0; i < n; i++) {
-            int curheight = heights[i];
+        int[] leftWalls = getLeftHeightIndcies(heights);
+        int[] rightWalls = getRightHeightIndices(heights);
 ​
-            while (stack.isEmpty() == false && heights[stack.peek()] >= curheight)
-                stack.pop();
+        int maxarea = 0;
 ​
-            if (stack.isEmpty() == true)
-                walls[i] = -1;
-            else
-                walls[i] = stack.peek();
+        for (int i = 0; i < heights.length; i++) {
+            int leftwall = leftWalls[i]; // left wall index
+            int rightWall = rightWalls[i]; // right wall index
 ​
-            stack.push(i);
-        }
+            int width = (rightWall - leftwall - 1);
+            int height = heights[i];
 ​
-        return walls;
-    }
-}
-​
+            int area = width * height;
+            maxarea = Math.max(maxarea, area);
