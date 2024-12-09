f59ee58d3cf1a4de456b6e406b87b3290262f213
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2024-12-09 22:50:34 +0530

    6 ms

--- solutions/787. Cheapest Flights Within K Stops.java
+++ solutions/787. Cheapest Flights Within K Stops.java
@@ -1,29 +1,50 @@
 class Solution {
-    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        List<List<int[]>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
-        for (int[] flight : flights) {
-            adj.get(flight[0]).add(new int[] {flight[1], flight[2]});
-        }
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {src, 0});
-        int[] minCost = new int[n];
-        Arrays.fill(minCost, Integer.MAX_VALUE);
-        int stops = 0;
-        while (!q.isEmpty() && stops <= k) {
-            int size = q.size();
-            while (size-- > 0) {
-                int[] curr = q.poll();
-                for (int[] neighbour : adj.get(curr[0])) {
-                    int price = neighbour[1], neighbourNode = neighbour[0];
-                    if (price + curr[1] >= minCost[neighbourNode]) continue;
-                    minCost[neighbourNode] = price + curr[1];
-                    q.offer(new int[] {neighbourNode, minCost[neighbourNode]});
-                }
-            }
-            stops++;
-        }
-        return minCost[dst] == Integer.MAX_VALUE ? -1 : minCost[dst];
-    }
-}
 ​
+    private static class Node implements Comparable<Node> {↔}
+​
+    public int findCheapestPrice(int totalFlights, int[][] flights, int src, int dst, int k) {
+        // TC: O(E log V * # flights) where E is the number of edges and V is the number
+        // of
+        // vertices.
+        // SC: O(V) because of distance map and visited map.
+​
+        /**
+         * There are n cities and m edges connected by some number of flights. You are
+         * given an array of flights where flights[i] = [ fromi, toi, pricei] indicates
+         * that there is a flight from city fromi to city toi with cost price. You have
+         * also given three integers src, dst, and k, and return the cheapest price from
+         * src to dst with at most k stops. If there is no such route, return -1.
+         */
+​
+        List<Node>[] graph = new List[totalFlights];
+​
+        for (int i = 0; i < totalFlights; i++) {
+            graph[i] = new ArrayList<>();
+        }
+​
+        // build the graph
+        for (int[] flight : flights) {
+            int u = flight[0];
+            int v = flight[1];
+            int w = flight[2];
+​
+            graph[u].add(new Node(0, v, w));
+        }
+​
+        // declare the linked list.
+        Queue<Node> pq = new LinkedList<>();
+        // declare the visited list
+        boolean[] visitedMap = new boolean[totalFlights];
+        int[] distanceMap = new int[totalFlights];
+​
+        // fill the distanceMap with 1e9
+        Arrays.fill(distanceMap, Integer.MAX_VALUE);
+​
+        // add the src into pq
+        pq.offer(new Node(0, src, 0));
+​
+        // perform dijsktra using Queue
+        while (pq.isEmpty() == false) {
+            Node node = pq.poll();
+            // System.out.println("visit:\t" + node);
+            if (node.steps > k)
