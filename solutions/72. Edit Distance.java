9f2dca62a1fe3c2a0245d5953ea58a1b0fb01d2a
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2024-12-22 09:38:46 +0530

    5 ms

--- solutions/72. Edit Distance.java
+++ solutions/72. Edit Distance.java
@@ -1,28 +1,36 @@
 class Solution {
-    private int[][] memo;
-    public int minDistance(String word1, String word2) {
-        if (word1.length() == 0) return word2.length();
-        if (word2.length() == 0) return word1.length();
-        int n = word1.length();
-        int m = word2.length();
-        memo = new int[n+1][m+1];
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= m; j++) {
-                if (i == 0)
-                    memo[i][j] = j;
-                else if (j == 0)
-                    memo[i][j] = i;
-                else if (word1.charAt(i-1) == word2.charAt(j-1)) {
-                    memo[i][j] = memo[i-1][j-1] + 0;
+    public int minDistance(String s, String t) {
+        int sn = s.length();
+        int tn = t.length();
+        
+        // int[][] dp = new int[sn+1][tn+1];
+        int[] prev=new int[tn+1];
+        int[] cur=new int[tn+1];
+        
+        // define base case
+        for (int i = 0; i < sn + 1; i++) {
+            cur[0] = i;
+        }
+        for (int j = 0; j < tn + 1; j++) {
+            prev[j] = j;
+        }
+        
+        // fill dp
+        for (int i=1; i < sn+1; i++) {
+            cur[0]=i;
+            for (int j=1; j < tn+1; j++) {
+                if (s.charAt(i-1)==t.charAt(j-1)) {
+                    cur[j]=prev[j-1];
                 } else {
-                    int a = memo[i][j-1];
-                    int b = memo[i-1][j];
-                    int c = memo[i-1][j-1];
-                    memo[i][j] = 1 + Math.min(a, Math.min(b, c));
+                    int op1 = prev[j]; // delete
+                    int op2 = cur[j - 1]; // insert
+                    int op3 = prev[j - 1]; // replace
+                    cur[j] = 1 + Math.min(op1, Math.min(op2, op3));
                 }
             }
+            prev=cur.clone();
         }
-        return memo[n][m];
+        
+        return prev[tn];
     }
-​
 }
