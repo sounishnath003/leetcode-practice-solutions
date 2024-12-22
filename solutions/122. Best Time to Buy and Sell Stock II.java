51dbe3bfb61e36e2f39bad73b709069f621dafea
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2024-12-22 12:21:48 +0530

    1 ms

--- solutions/122. Best Time to Buy and Sell Stock II.java
+++ solutions/122. Best Time to Buy and Sell Stock II.java
@@ -1,8 +1,8 @@
-class Solution {
-    public int maxProfit(int[] arr) {
+         // EASY: O(N) solution
+         // 
         final int n = arr.length;
         int overrallProfit = 0;
-​
+•
         for (int i = 1; i < n; i++) {
             int profit = arr[i] - arr[i-1];
             if (profit > 0) {
@@ -12,4 +12,37 @@ class Solution {
         
         return overrallProfit;
     }
-}
+    
+    public int maxProfit(int[] prices) {
+        // return recursive(prices, 0, true);
+            int n = prices.length;
+            int[][] dp = new int[n + 1][2];
+​
+            // base case n == prices.length
+            dp[n][0] = dp[n][1] = 0;
+​
+            // fill the dp.
+            for (int i = n - 1; i >= 0; i--) {
+                for (int buy = 0; buy < 2; buy++) {
+                    // buy : true
+                    if (buy == 1) {
+                        // Math.max((-1 * prices[i]) + recursive(prices, i + 1, false),
+                        // recursive(prices, i + 1, true));
+                        dp[i][buy] = Math.max(
+                                (-1 * prices[i]) + dp[i + 1][0],
+                                dp[i + 1][1]);
+                    } else {
+                        // sell : false
+                        // if i sell, i will get the current price
+                        // op2 = prices[i] + recursive(prices, i + 1, true);
+                        // // if i skip, i will not get anything
+                        // op3 = recursive(prices, i + 1, false);
+                        dp[i][buy] = Math.max(
+                                prices[i] + dp[i + 1][1],
+                                dp[i + 1][0]);
+                    }
+                }
+            }
+​
+            return dp[0][1];
+    }
