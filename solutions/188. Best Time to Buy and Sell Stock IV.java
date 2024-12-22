edecf8ef34bcb3088341fea9b8b56cdabfd733fb
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2024-12-22 16:05:34 +0530

    8 ms

--- solutions/188. Best Time to Buy and Sell Stock IV.java
+++ solutions/188. Best Time to Buy and Sell Stock IV.java
@@ -1,30 +1,49 @@
 class Solution {
-    public int maxProfit(int k, int[] prices) {
-        int n = prices.length;
-        if(k==0||n==0) return 0;
-        if(k >= n/2){
-            //it same as infinite tries
-            int maxProfit = 0;
-            for(int i=0;i<n-1;i++){
-                //means either we dont buy or sell or buy today sell Tomorrow
-                maxProfit = maxProfit + Math.max(0,prices[i+1]-prices[i]);
-            }
-            return maxProfit;
-        }
-        
-        int[] buy = new int[k];
-        int[] sell = new int[k];
-        Arrays.fill(buy,Integer.MIN_VALUE);
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<k;j++){
-                //j==0 sybolises that this is our fisrst txn
-                //sell[j-1] - prices[j]: means the money we have from last txn - prices of stock today
-                buy[j] = Math.max(buy[j],j==0?0-prices[i]:sell[j-1]-prices[i]);
-                sell[j] = Math.max(sell[j],buy[j]+prices[i]);
-                //Max of either we don't do any thing or we make profit today
-            }
-        }
-        return sell[k-1];   
+  public int maxProfit(int k, int[] prices) {
+    int n = prices.length;
+    // define the dp struct.
+    int[][][] dp = new int[n + 1][2][k+1];
+    // fill the junk values for memoization.
+    for (int row[][]: dp)
+      for (int ir[]: row) Arrays.fill(ir, -1);
+    // base case cond.
+    dp[0][1][0] = 0;
+​
+    return recursive(prices, 0, 1, k, dp);
+  }
+​
+  public int recursive(int[] prices, int i, int buy, int transaction, int[][][] dp) {
+    // base conds.
+    if (i == prices.length) {
+      return 0;
     }
+    if (transaction == 0) {
+      return 0;
+    }
+    if (dp[i][buy][transaction] != -1) {
+      return dp[i][buy][transaction];
+    }
+​
+    // options
+    int op1 = 0, op2 = 0, op3 = 0;
+​
+    // if asked to buy, i need to buy the current cost or do thing (skip)
+    if (buy == 1) {
+      // i am paying from my pocket
+      // buy the stock
+      // skip the stock
+      op1 = Math.max(
+        -prices[i] + recursive(prices, i + 1, 0, transaction, dp),
+        recursive(prices, i + 1, 1, transaction, dp));
+    } else {
+      // i need to sell,
+      op2 = prices[i] + recursive(prices, i + 1, 1, transaction - 1, dp);
+      // or do not do anything
+      op3 = recursive(prices, i + 1, 0, transaction, dp);
+    }
+​
+    int profit = Math.max(op1, Math.max(op2, op3));
+    dp[i][buy][transaction] = profit;
+    return profit;
+  }
 }
