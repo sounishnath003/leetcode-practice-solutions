feff1fcb02ccbb2f1c80d960291dbe6604091b0e
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-03-09 09:09:03 +0530

    Time: 1 ms (63.54%) | Memory: 45.2 MB (40.02%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,16 +1,15 @@
-class Solution {
-
+public class Solution {
     // https://leetcode.com/problems/is-graph-bipartite/
     public boolean isBipartite(int[][] graph) {
         int nodes = graph.length;
         int[] colors = new int[nodes];
         Arrays.fill(colors, -1); // Uncolored node
 
-        // Explore all the ndoes and color it
+        // Explore all the nodes and color it
         for (int node = 0; node < nodes; node++) {
             // Pick the uncolored node
             if (colors[node] == -1) {
-                boolean isColorable = this.colorIt(graph, node, colors, 0);
+                boolean isColorable = this.colorItWithBFS(graph, node, colors, 0);
                 if (!isColorable)
                     return false;
             }
@@ -19,7 +18,7 @@ class Solution {
         return true;
     }
 
-    private boolean colorIt(int[][] graph, int node, int[] colors, int color) {
+    private boolean colorItWithDFS(int[][] graph, int node, int[] colors, int color) {
         // Apply the main conditional check
         // If the node is already color and it is same color
         // Simply return False
@@ -31,7 +30,7 @@ class Solution {
         colors[node] = color;
         // Explore all neigh and try to color them with opposite color
         for (int neigh : graph[node]) {
-            if (!this.colorIt(graph, neigh, colors, 1 - color)) {
+            if (!this.colorItWithDFS(graph, neigh, colors, 1 - color)) {
                 return false;
             }
         }
@@ -39,4 +38,25 @@ class Solution {
         return true;
     }
 
+    private boolean colorItWithBFS(int[][] graph, int startNode, int[] colors, int color) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(startNode);
+        colors[startNode] = color;
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            for (int neigh : graph[node]) {
+                if (colors[neigh] == -1) {
+                    // Color the neighbor with opposite color
+                    colors[neigh] = 1 - colors[node];
+                    queue.add(neigh);
+                } else if (colors[neigh] == colors[node]) {
+                    // If the neighbor is already colored with the same color, return false
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
 }
