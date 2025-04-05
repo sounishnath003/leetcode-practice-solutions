b24e9649ff6959fe4fb721393e509e72a5893944
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-04-05 22:14:47 +0530

    Time: 403 ms (19.19%) | Memory: 23.3 MB (22.99%) - LeetSync

--- 1503-reducing-dishes/reducing-dishes.py
+++ 1503-reducing-dishes/reducing-dishes.py
@@ -1,3 +1,7 @@
+# https://leetcode.com/problems/reducing-dishes/description/
+
+from typing import List
+
 class Solution:
     def maxSatisfaction(self, arr: List[int]) -> int:
         """recursion formula was: simply"""
@@ -22,3 +26,22 @@ class Solution:
                 )
                 
         return max(0, dp[0][1])  # return max of 0 and result starting from first dish
+       
+    def maxSatisfactionRec(self, arr: List[int]) -> int:
+        """we can apply pick and not pick concept"""
+        def f(i, time, dp=None):
+            if i == len(arr):
+                return 0
+            if (i, time) in dp:
+                return dp[(i, time)]
+            # not pick
+            notp = 0 + f(i + 1, time, dp)
+            # pick
+            pick = (time * arr[i]) + f(i + 1, time + 1, dp)
+
+            dp[(i, time)] = max(notp, pick)
+            return dp[(i, time)]
+
+        dp = {}
+        arr.sort()
+        return f(0, 1, dp)
