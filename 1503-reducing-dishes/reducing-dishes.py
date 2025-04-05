4bb70b0052282ee654af5b872261dafa796c6c1f
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-04-05 21:04:58 +0530

    Time: 767 ms (5.45%) | Memory: 43.4 MB (8.77%) - LeetSync

---
+++ 1503-reducing-dishes/reducing-dishes.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxSatisfaction(self, arr: List[int]) -> int:
+        """we can apply pick and not pick concept"""
+        def f(i,time,dp=None):
+            if dp is None:
+                dp={}
+            if i == len(arr):
+                return 0
+            if (i,time) in dp:
+                return dp[(i,time)]
+            # not pick
+            notp=0+f(i+1,time,dp)
+            # pick
+            pick=(time * arr[i]) + f(i+1,time+1,dp)
+
+            dp[(i,time)]=max(notp,pick)
+            return dp[(i,time)]
+
+        arr.sort()
+        return f(0,1)
+        
