55180b1238a91666be8e81fd250490586438dae4
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-04-06 17:59:41 +0530

    Time: 107 ms (10.28%) | Memory: 18.4 MB (47.50%) - LeetSync

--- 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.py
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.py
@@ -7,6 +7,22 @@ class Solution:
         m = len(strs[0]) # length of word # columns
 
         delete=0
+        for ci in range(m):
+            for ri in range(n-1):
+                if ord(strs[ri][ci]) > ord(strs[ri+1][ci]):
+                    delete+=1
+                    break
+
+        return delete
+
+    def minDeletionSizeSlow(self, strs: List[str]) -> int:
+        def is_sorted(l):
+            return all(a <= b for a, b in zip(l, l[1:]))
+
+        n = len(strs) # given words # rows
+        m = len(strs[0]) # length of word # columns
+
+        delete=0
         seen={}
         for c in range(m):
             l=[ strs[r][c] for r in range(n) ]
