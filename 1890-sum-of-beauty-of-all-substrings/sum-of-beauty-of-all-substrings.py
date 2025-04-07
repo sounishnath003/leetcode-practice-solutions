b0af1cae08d05d3d6fa56413f9e6bbc3f31e9e77
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-04-07 22:46:36 +0530

    Time: 1194 ms (89.50%) | Memory: 17.9 MB (18.24%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.py
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.py
@@ -1,5 +1,22 @@
 class Solution:
     def beautySum(self, s: str) -> int:
+        n = len(s)
+        total_beauty_sum = 0
+
+        for start in range(n):
+            char_freq = {}  # Frequency of characters in the current substring
+
+            for end in range(start, n):
+                char = s[end]
+                char_freq[char] = char_freq.get(char, 0) + 1
+
+                min_freq=min(char_freq.values())
+                max_freq=max(char_freq.values())
+                total_beauty_sum += (max_freq - min_freq)
+
+        return total_beauty_sum
+
+    def beautySumSlow(self, s: str) -> int:
         maxf=-float("inf")
         minf=float("inf")
         charfreq={}
