947cd8d1624c645f36d9e89a2cb3064ab48aea98
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-04-17 14:01:53 +0530

    Time: 34 ms (84.94%) | Memory: 17.8 MB (57.19%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.py
@@ -0,0 +1,25 @@
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        MIN=-1 * (2**31)
+        MAX=(2**31) - 1
+        
+        def check_overflow(x):
+            return True if x < MIN or x > MAX else False
+        
+        if check_overflow(x):
+            return 0
+        
+        is_negetive=x < 0
+        num=0
+        if is_negetive:
+            x=abs(x)
+        
+        while x > 0:
+            num = num * 10 + (x % 10)
+            x=x//10
+            
+        num=-1 * num if is_negetive else num
+        return 0 if check_overflow(num) else num
+            
+        
