b698c1e46b4f549bae1a5a5a124f6330f85495f3
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-04-28 13:31:33 +0530

    Time: 30 ms (43.86%) | Memory: 18 MB (19.44%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -2,6 +2,23 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         s1n=len(s1)
         s2n=len(s2)
+        s1m=Counter(s1)
+        s2m=defaultdict(int)
+        for i in range(s2n):
+            c1=s2[i]
+            s2m[c1]+=1
+            if s1m==s2m:
+                return True
+            if i-s1n+1 >= 0 and s2m[s2[i-s1n+1]]:
+                s2m[s2[i-s1n+1]] -=1
+                if s2m[s2[i-s1n+1]] == 0:
+                    del s2m[s2[i-s1n+1]]
+
+        return False
+            
+    def checkInclusion1(self, s1: str, s2: str) -> bool:
+        s1n=len(s1)
+        s2n=len(s2)
         s1m=[0] * 26
         for i, c in enumerate(s1):
             s1m[ord(c) - ord('a')] += 1
