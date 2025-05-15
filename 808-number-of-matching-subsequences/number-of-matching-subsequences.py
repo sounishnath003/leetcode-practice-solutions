1287af1c6163e273458fda59ab175fbb9fd20e1d
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-05-15 09:04:33 +0530

    Time: 760 ms (30.50%) | Memory: 25.1 MB (6.05%) - LeetSync

--- 808-number-of-matching-subsequences/number-of-matching-subsequences.py
+++ 808-number-of-matching-subsequences/number-of-matching-subsequences.py
@@ -1,48 +1,13 @@
 
-class Solution:
-    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
-        # bruteforce soln:
-            # 1. generate all subsequence of a string and count++
-        # optimzied:
-            # 1. implement the trie of the words provided
-            # 2. search the indexes everytime in the trie
-        
-        root = {}
-
-        def insert(word: str) -> None:
-            node=root
-            for char in word:
-                if char not in node:
-                    node[char] = {"count":  0}
-                node=node[char]
-            
-            node["count"] +=1
-
-        # for all word in words store into the trie
-        for word in words:
-            insert(word)
-
-        def find_matching(s: str, i: int, node: dict) -> int:
-            ans  = node["count"] if "count" in node else 0
-
-            if i >= len(s):
-                return ans
-            
-            for c in string.ascii_lowercase:
-                if c in node:
-                    try:
-                        index = s.index(c, i)
-                        ans += find_matching(s, i+1, node[c])
-                    except Exception:
-                        continue
-            
-            return ans
-        
-
-        return find_matching(s,0,root)
 
 class Solution:
   def numMatchingSubseq(self, s: str, words: list[str]) -> int:
+    # bruteforce soln:
+        # 1. generate all subsequence of a string and count++
+    # optimzied:
+        # 1. implement the trie of the words provided
+        # 2. search the indexes everytime in the trie
+    
     root = {}
 
     def insert(word: str) -> None:
