50547cd8f0a232d33284622f4b42c4e56cde3bd7
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-05-23 10:58:36 +0530

    Time: 193 ms (18.45%) | Memory: 19.1 MB (29.66%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,36 +1,28 @@
 import math
-
 class Solution:
-    def minEatingSpeed(self, piles: list[int], h: int) -> int:
-        """
-        koko wants to finish banana at K speed, K must be minimum
-        within H hours
-        -> binary search
-        """
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # we can apply binary search
+        # as we need to minimize the maximum
+        if len(piles) < 1:
+            return max(piles)
         low = 1
         high = max(piles)
         ans = high
 
-        def possible_to_finish(banana_speed, hour_limit):
-            # speed at koko should be eating is banana_speed
-            res = 0
+        def possible(banana):
+            res=0
             for pile in piles:
-                # Use math.ceil to correctly calculate hours needed for each pile
-                res += math.ceil(pile / banana_speed)
-                if res > hour_limit:
+                res += math.ceil(pile / banana)
+                if res > h:
                     return False
             return True
 
-        # we will search between low to high range
         while low <= high:
-            # we will try to eat mid banana
-            atleast_banana = (low + high) // 2 # Integer division for mid calculation
-            # check if koko can finish at least banana within H hours
-            # if he can, we will try to minimize the search space by making high=mid-1
-            if possible_to_finish(atleast_banana, h):
-                ans = atleast_banana # Update ans directly, no need for min with itself if we're narrowing the search space
-                high = atleast_banana - 1
+            mid = math.ceil((low + high) / 2)
+            if possible(mid):
+                ans=mid
+                high=mid-1
             else:
-                low = atleast_banana + 1
+                low=mid+1
 
         return ans
