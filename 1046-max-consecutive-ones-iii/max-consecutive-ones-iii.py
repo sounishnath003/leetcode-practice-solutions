d69aaf28fc25d263e6eb16e987192286379cbbda
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-05-31 13:03:54 +0530

    Time: 71 ms (37.09%) | Memory: 18.4 MB (15.09%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -1,26 +1,40 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        def max_consecutive_ones_III(arr, k):
-            n=len(arr)
-            gmax=0
-            count=0
-            start=0
+        """
+        what if?
+        1) count zeros in your iteration 0...n
+        2) when countOfZeros > k:
+            2.1) try to shrink the window and 
+            2.2) calc the window length
+            2.3) decrement the countOfZeros
+            2.4) increment the start pointer
+        3) maintain a global window and update it
+        4) return the global window as answer
+        """
 
-            for end in range(n):
-                e=arr[end]
-                if e == 0:
-                    # when it is 0, we need to flip to 1
-                    count+=1
-                while count > k:
-                    # we need to backtrack the start back to 0
-                    # only if it is 0
-                    if arr[start] == 0:
-                        count -=1
-                    start+=1
+        # get the length of arr
+        n=len(nums)
+        # define the vars
+        start=0
+        zero_count=0
+        gans=0
 
-                window_length=end-start+1
-                gmax=max(gmax, window_length)
-
-            return gmax
-
-        return max_consecutive_ones_III(nums,k)
+        # iterate over the window
+        for end, elem in enumerate(nums):
+            # check if elem == 0
+            if elem == 0:
+                # incr zero count
+                zero_count +=1
+            # check if you surpassed the K
+            while zero_count > k:
+                # shirnk the window and update the incr
+                # in case it is zero
+                if nums[start]==0:
+                    zero_count -= 1
+                start += 1
+            # just update the global window ans
+            # as per the start .... end window
+            gans = max(gans, end-start + 1) # added (1) - 0 based indexing
+            
+            
+        return gans
