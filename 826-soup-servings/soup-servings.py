ef7fef4743e749bb2899fe5fc605cab0cd7d345c
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-05-31 12:08:21 +0530

    Time: 8 ms (77.67%) | Memory: 20 MB (49.03%) - LeetSync

---
+++ 826-soup-servings/soup-servings.py
@@ -0,0 +1,27 @@
+class Solution:
+    def soupServings(self, n: int) -> float:
+        
+        @cache
+        def f(soupa, soupb):
+            """
+            4 kind of ops we can perform
+            """
+            if n > 4800:
+                return 1.0
+            # base case: both soup is empty we stop 
+            if soupa <= 0 and soupb <= 0:
+                return 0.50
+            # if soupa <= 0
+            if soupa <= 0:
+                return 1.0
+            if soupb <= 0:
+                return 0.0
+
+            op1=0.25 * f(soupa - 100, soupb - 0)
+            op2=0.25 * f(soupa - 75, soupb - 25)
+            op3=0.25 * f(soupa - 50, soupb - 50)
+            op4=0.25 * f(soupa - 25, soupb - 75)
+
+            return op1+op2+op3+op4
+
+        return f(n,n)
