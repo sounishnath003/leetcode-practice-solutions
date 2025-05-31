376dc457879ee8a92f7c0e96d732ab7885b5e76b
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-05-31 17:24:15 +0530

    Time: 3 ms (47.36%) | Memory: 18.6 MB (35.70%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.py
+++ 941-sort-array-by-parity/sort-array-by-parity.py
@@ -1,3 +1,15 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        j=0
+        for i, e in enumerate(nums):
+            if e % 2 == 1:
+                continue
+            # but if it is even then
+            nums[i],nums[j]=nums[j],nums[i]
+            j+=1
+
+        return nums
+    
+    def bruteforce(self, nums: List[int]) -> List[int]:
+        """because of python magic"""
         return [ x for x in nums if x % 2 == 0 ] + [ x for x in nums if x % 2 == 1 ]
