a1a4474a8a0aa184ceee0724ea864f521ce5517d
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-06-14 20:06:55 +0530

    Time: 987 ms (62.40%) | Memory: 18.1 MB (10.40%) - LeetSync

---
+++ 1723-maximum-number-of-achievable-transfer-requests/maximum-number-of-achievable-transfer-requests.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maximumRequests(self, n: int, requests: List[List[int]]) -> int:
+        max_requests=0
+        movements=[0 for _ in range(n)]
+
+        def backtrack(index, count):
+            nonlocal max_requests
+            nonlocal movements
+
+            if index == len(requests):
+                if all(d==0 for d in movements):
+                    max_requests=max(max_requests, count)
+                return
+            
+            fromm, to=requests[index]
+            movements[fromm] -=1
+            movements[to] +=1
+
+            backtrack(index+1, count + 1)
+            # backtrack
+            movements[fromm] +=1
+            movements[to] -=1
+
+            backtrack(index+1, count)
+
+        backtrack(0,0)
+        return max_requests
