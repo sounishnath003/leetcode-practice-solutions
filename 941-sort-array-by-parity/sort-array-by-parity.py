de2a4fc396e6ad9a70bd808d4c9af4db19e53d46
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-08-02 20:35:20 +0530

    Time: 4 ms (18.09%) | Memory: 18.3 MB (81.65%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.py
+++ 941-sort-array-by-parity/sort-array-by-parity.py
@@ -1,15 +1,27 @@
+
 class Solution:
-    def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        j=0
-        for i, e in enumerate(nums):
-            if e % 2 == 1:
-                continue
-            # but if it is even then
-            nums[i],nums[j]=nums[j],nums[i]
-            j+=1
+    def sortArrayByParity(self, nums: list[int]) -> list[int]:
+        """we need to place all evens first part, then all the odds"""
+        left=0
+        right=len(nums) - 1
+
+        def is_even(n:int):
+            return n%2==0
+
+        while left < right:
+            left_elem=nums[left]
+            right_elem=nums[right]
+
+            if is_even(left_elem):
+                # nothing to do, just move your left pointer
+                left +=1
+            elif not is_even(right_elem):
+                # nothing to do, just move your right pointer
+                right -= 1
+            else:
+                # swap the positions
+                nums[left],nums[right]=right_elem,left_elem
+                left +=1
+                right -=1
 
         return nums
-    
-    def bruteforce(self, nums: List[int]) -> List[int]:
-        """because of python magic"""
-        return [ x for x in nums if x % 2 == 0 ] + [ x for x in nums if x % 2 == 1 ]
