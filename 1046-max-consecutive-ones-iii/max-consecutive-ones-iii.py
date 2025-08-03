7d1b35aea003fd49534710bcedf9ff05c2034444
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-08-03 15:25:24 +0530

    Time: 82 ms (19.31%) | Memory: 20.4 MB (8.50%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -1,5 +1,34 @@
 class Solution:
-    def longestOnes(self, nums: List[int], k: int) -> int:
+    def longestOnes(self, nums: List[int], k:int) -> int:
+        """returns longest windows ones"""
+        # ret. max consecutive ones. if you can flip atmost k zeros.
+        # get the length of the arr
+        n=len(nums)
+        # i can count zeros in my current iteration
+        # whenever i encountered more than K zeros,
+        # shrink the window -> get the length of the window -> zero-- -> increment start pointer
+        # calc. the max(curr.window, global.window)
+        # return the global.window
+
+        zero=0
+        start=0
+        max_window=0
+
+        for end, elem in enumerate(nums):
+            if elem==0:
+                zero+=1
+
+            while zero > k:
+                if nums[start] == 0:
+                    zero-=1
+                start+=1
+            
+            window=(end-start+1)
+            max_window=max(max_window, window)
+
+        return max_window
+        
+    def longestOnes2(self, nums: List[int], k: int) -> int:
         """
         what if?
         1) count zeros in your iteration 0...n
@@ -35,6 +64,5 @@ class Solution:
             # just update the global window ans
             # as per the start .... end window
             gans = max(gans, end-start + 1) # added (1) - 0 based indexing
-            
             
         return gans
