7c0de8e4531c4e03cb9361834f47904f087e78af
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-09-13 00:03:11 +0530

    Time: 40 ms (25.25%) | Memory: 29.2 MB (90.57%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,18 +1,20 @@
 class Solution:
-    def findDuplicate(self, arr: List[int]) -> int:
-        """ apply the cyclic sorting """
-        slow=arr[0]
-        fast=arr[0]
+    def findDuplicate(self, nums: List[int]) -> int:
+        """simply apply the 2 pointer technique
+        tortoise and hare or slow or fast pointers"""
+
+        slow=nums[0]
+        fast=nums[0]
 
         while True:
-            slow=arr[slow]
-            fast=arr[arr[fast]]
+            slow = nums[slow]
+            fast = nums[nums[fast]]
             if slow == fast:
                 break
 
-        fast=arr[0]
-        while slow!=fast:
-            slow=arr[slow]
-            fast=arr[fast]
+        fast=nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
 
         return slow
