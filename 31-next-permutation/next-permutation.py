083b6cd5588be1f7084a14e1ee7fdca3771e1749
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-09-13 20:29:58 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (85.97%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -1,27 +1,48 @@
-from typing import List
+import typing
 
+# LC 31: https://leetcode.com/problems/next-permutation/
 class Solution:
-    def nextPermutation(self, nums: List[int]) -> None:
+    def nextPermutation(self, nums: typing.List[int]) -> typing.List[int]:
         """
-        Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-memory  instead.
         """
-        # Find the first decreasing element from the right
-        n = len(nums)
-        i = n - 2
-        while i >= 0 and nums[i] >= nums[i + 1]:
-            i -= 1
-
-        # If such an element exists, find the rightmost element to swap with it
-        if i >= 0:
-            j = n - 1
-            while j >= 0 and nums[j] <= nums[i]:
-                j -= 1
-            # Swap the two elements
+        def swap(nums, i: int, j: int) -> None:
+            """utility method: swap the position (i,j) for a given list"""
+            # use python magic in one-line replace.
             nums[i], nums[j] = nums[j], nums[i]
 
-        # Reverse the suffix starting from i + 1
-        left, right = i + 1, n - 1
-        while left < right:
-            nums[left], nums[right] = nums[right], nums[left]
-            left += 1
-            right -= 1
+        def reverse(nums, start:int, end:int) -> None:
+            """utility method: returns a given list in range (start, end)"""
+            while start < end:
+                swap(nums, start, end)
+                start += 1
+                end -= 1
+
+        # get the length of the arr
+        n = len(nums)
+        # find a pivot element, where nums[i+1] > nums[i]
+        first_index = None
+
+        for i in range(n-2, -1, -1):
+            if nums[i+1] > nums[i]:
+                first_index = i
+                break
+
+        # if still i could not find any first index where nums[i+1] > nums[i]
+        # it is already sorted in DESC; in that case my answer'd be reverse of nums itself
+        # e.g. 4321 -> 1234: as mentioned in question.
+        if first_index is None:
+            reverse(nums, 0, n-1)
+            return # void return
+        
+        # find the second index; i.e. nums[index] > nums[first_index]
+        second_index = n-1
+        for i in range(n-1, first_index, -1):
+            if nums[i] > nums[first_index]:
+                second_index = i
+                break
+        # now, as you got the two pivotal element you just have to swap them
+        # 1342 -> fi: 3, si: 4 -> 1432 -> 1423
+        swap(nums, first_index, second_index)
+        # then, you have to just reverse the sequence from first_index + 1 .... end
+        reverse(nums, first_index + 1, n-1)
