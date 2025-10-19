9e44defd3b8f9639c06ad5ff6438a38c50cc0127
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-10-19 21:36:09 +0530

    Time: 193 ms (19.46%) | Memory: 18 MB (49.20%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,19 +1,18 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        freqmap = {}
+        """return the longest window of characters you can 
+        get after performing the operations at most K times"""
         start = 0
-        res = 0
-        maxf=0
-        for end in range(len(s)):
-            freqmap[s[end]] = 1 + freqmap.get(s[end], 0)
-            maxf=max(maxf, freqmap[s[end]])
-            window_length = end - start + 1
-            while window_length - maxf > k:
-                freqmap[s[start]] -= 1
-                if freqmap[s[start]] == 0:
-                    del freqmap[s[start]]  # Remove key when count is zero
-                start += 1
-                window_length = end - start + 1  # Update window length
-            res = max(res, window_length)
+        charmap = defaultdict(int)
+        mwindow = 0
 
-        return res
+        # iterate over the string
+        for end, char in enumerate(s):
+            charmap[char] += 1
+            # while
+            while (end - start + 1) - int(max(charmap.values())) > k:
+                charmap[s[start]] -= 1
+                start += 1
+            mwindow = max(mwindow, (end - start + 1))
+
+        return mwindow
