046c1d0126d894867d92a32c8dc01ce1441930b2
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-10-20 14:29:25 +0530

    Time: 13 ms (87.94%) | Memory: 17.8 MB (87.80%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,35 +1,24 @@
 class Solution:
-    def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1n=len(s1)
-        s2n=len(s2)
-        s1m=Counter(s1)
-        s2m=defaultdict(int)
-        for i in range(s2n):
-            c1=s2[i]
-            s2m[c1]+=1
-            if s1m==s2m:
-                return True
-            if i-s1n+1 >= 0 and s2m[s2[i-s1n+1]]:
-                s2m[s2[i-s1n+1]] -=1
-                if s2m[s2[i-s1n+1]] == 0:
-                    del s2m[s2[i-s1n+1]]
+    def checkInclusion(self, s: str, t: str) -> bool:
+        sn = len(s)
+        tn = len(t)
+        # t is the string i will be iterate on
+        if sn > tn:
+            return False
 
-        return False
-            
-    def checkInclusion1(self, s1: str, s2: str) -> bool:
-        s1n=len(s1)
-        s2n=len(s2)
-        s1m=[0] * 26
-        for i, c in enumerate(s1):
-            s1m[ord(c) - ord('a')] += 1
+        start = 0
+        smap = Counter(s)
+        tmap = defaultdict(int)
 
-        for i in range(s2n-s1n+1):
-            t=s2[i:i+s1n]
-            tm = [0] * 26
-            for i, c in enumerate(t):
-                tm[ord(c)-ord('a')] +=1
-
-            if tm==s1m:
-                return True
-
+        for end in range(tn):
+            tmap[t[end]] += 1
+            window = (end - start + 1)
+            if window == sn:
+                # if all([True if tmap[k] == smap[k] else False for k in smap.keys()]):
+                if tmap == smap:
+                    return True
+                tmap[t[start]] -= 1
+                if tmap[t[start]] == 0:
+                    del tmap[t[start]]
+                start += 1
         return False
