da7a2189bb9df02d5964505dc31b90f27ff30513
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-10-21 10:36:22 +0530

    Time: 15 ms (84.28%) | Memory: 18.1 MB (19.39%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -2,23 +2,36 @@ class Solution:
     def checkInclusion(self, s: str, t: str) -> bool:
         sn = len(s)
         tn = len(t)
-        # t is the string i will be iterate on
-        if sn > tn:
+        # check if the sn is bigger than the tn
+        if tn < sn:
+            # it is not possible that the possible match is longer 
+            # than the string i will be looking at; hence return False
             return False
 
+        # declare two pointers
         start = 0
+        # declare some mapps of characters to store
         smap = Counter(s)
-        tmap = defaultdict(int)
-
+        tmap = defaultdict(int) # default dict: char -> frequency.int
+        # iterate over the t string and check for the window of sn length
         for end in range(tn):
-            tmap[t[end]] += 1
+            tchar = t[end]
+            # put it in tmap
+            tmap[tchar] += 1
+            # check the window length
             window = (end - start + 1)
+            # if you have hit the same sn length window now check followings
             if window == sn:
-                # if all([True if tmap[k] == smap[k] else False for k in smap.keys()]):
+                # if both map has equal number of characters stored
                 if tmap == smap:
                     return True
-                tmap[t[start]] -= 1
+                # also remove the t[start] count from the tmap
+                tmap[t[start]] -=1
+                # if the count is 0 then remove the key from the tmap
                 if tmap[t[start]] == 0:
                     del tmap[t[start]]
+                # increment the start pointer
                 start += 1
+
+        # no observation of having the permutation present in the tn
         return False
