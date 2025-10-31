136dbb491bfd39dab97b6bedff66906e407073f8
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-10-31 18:25:21 +0530

    Time: 3 ms (30.61%) | Memory: 18 MB (69.86%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+
+        def fn(open_bracket:int, close_bracket: int, sequence:str) -> None:
+            nonlocal n, res
+            if open_bracket > n or len(sequence) > 2*n: return
+
+            if len(sequence) == 2 * n:
+                res.append(sequence)
+                return
+
+            fn(open_bracket+1, close_bracket, sequence+'(')
+            if open_bracket > close_bracket:
+                fn(open_bracket, close_bracket+1, sequence+')')
+
+
+        fn(0,0,"")
+
+        return res
