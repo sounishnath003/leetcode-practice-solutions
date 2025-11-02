d17606c2d82005e639c31510dbd770965d4a13ac
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-11-02 20:53:52 +0530

    Time: 43 ms (57.52%) | Memory: 18.3 MB (15.99%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -1,31 +1,18 @@
-# Definition for a Node.
-class Node:
-    def __init__(self, val=0, neighbors=None):
-        self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
-
-from typing import Optional
-
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        if node is None:
-            return None
+        node_map = defaultdict(Node)
 
-        self.node_maps = {}  # Initialize node_maps within cloneGraph
-        return self._clone(node)
+        def clone(node: "Node"):
+            nonlocal node_map
+            if not node: return None
 
-    def _clone(self, node: Node) -> Optional['Node']:
-        if node is None:
-            return None
+            if node in node_map: return node_map[node]
+            
+            node_map[node] = Node(node.val)
+            for neigh in node.neighbors:
+                cloned_neigh = clone(neigh)
+                node_map[node].neighbors.append(cloned_neigh)
 
-        if node in self.node_maps:  # Check if node is already cloned
-            return self.node_maps[node]
+            return node_map[node]
 
-        nnode = Node(node.val)
-        self.node_maps[node] = nnode  # Store the cloned node immediately
-
-        for nei in node.neighbors:
-            neinode = self._clone(nei)
-            nnode.neighbors.append(neinode)
-
-        return nnode
+        return clone(node)
