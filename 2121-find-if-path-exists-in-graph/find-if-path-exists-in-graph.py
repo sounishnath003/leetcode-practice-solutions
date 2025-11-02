90b05699cc827d4cd2d0ffceb29b426064d5741b
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-11-02 14:08:34 +0530

    Time: 571 ms (48.21%) | Memory: 141.3 MB (12.17%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -1,10 +1,39 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        """Solving using backtracking DFS"""
+        
+        def build_graph(edges: List[List[int]]) -> Dict[int, List[int]]:
+            """helps to build a graph from the lists of edges"""
+            graph = defaultdict(list)
+            for u, v in edges:
+                graph[u].append(v)
+                graph[v].append(u)
+
+            return graph
+
+        graph = build_graph(edges)
+
+        def dfs(node: int, visited: Set[int]) -> bool:
+            """recursively go into their neighbours and check if we can reach the destination"""
+            nonlocal graph
+            if node == destination: return True
+            visited.add(node)
+
+            for neighbour in graph[node]:
+                if neighbour not in visited and dfs(neighbour, visited): return True
+            
+            return False
+
+        visited = set()
+        return dfs(source, visited)
+
+
+    def validPath2(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
         class UnionFind:
             def __init__(self, nodes:int) -> None:
-                self.nodes = nodes
+                self.nodes = int
                 self.parents = list(range(nodes+1))
-                self.rank = [1] * (nodes+1)
+                self.rank = [1] * (self.nodes+1)
 
             def union(self, u:int, v:int) -> None:
                 up = self.parents[u]
