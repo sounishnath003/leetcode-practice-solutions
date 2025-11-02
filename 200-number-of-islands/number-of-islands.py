5b0121afdbf8f8692d3d77a0249f82daca67677e
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-11-02 14:57:33 +0530

    Time: 282 ms (20.01%) | Memory: 28.3 MB (11.59%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,41 +1,23 @@
 class Solution:
-    def dfs(self, grid, row:int, col:int, R:int, C:int, visited:set) -> None:
-        # visit the cell
-        visited.add((row,col))
-        # traverse all the neighbours and find all
-        # you can only go 4 directions
-        dr=[-1,0,1,0]
-        dc=[0,1,0,-1]
-
-        for k in range(4):
-            nrow=row+dr[k]
-            ncol=col+dc[k]
-
-            # is safe then only go
-            if nrow < 0 or nrow >= R or ncol < 0 or ncol >= C or grid[nrow][ncol]== '0' or ((nrow,ncol) in visited):
-                continue
-
-            self.dfs(grid,nrow,ncol,R,C,visited)
-
-        return 
-
     def numIslands(self, grid: List[List[str]]) -> int:
-        # get the rows and cols
-        R,C=len(grid),len(grid[0])
-        # store the visited nodes into a set
-        visited=set()
-        # store count the islands
-        islands=0
-        # traverse all rows,cols where it is a land only
+        R = len(grid)
+        C = len(grid[0])
+
+        islands = 0
+        visited = set()
+
+        def dfs(row: int, col: int):
+            if not (0 <= row < R and 0 <= col < C) or grid[row][col] == "0" or (row, col) in visited: return 0
+            
+            visited.add((row, col))
+
+            for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                dfs(row + dr, col + dc)
+                
+            return 1
+
         for row in range(R):
             for col in range(C):
-                # cell is 0; it is water; skip that
-                # also check, is (row,col) is not yet visited!! IMPORTANT
-                if grid[row][col]=='0' or (row,col) in visited:
-                    continue
-                # perform a DFS
-                self.dfs(grid, row, col, R, C, visited)
-                # increment the count of islands
-                islands += 1
+                islands += dfs(row, col) if grid[row][col] == "1" else 0
 
         return islands
