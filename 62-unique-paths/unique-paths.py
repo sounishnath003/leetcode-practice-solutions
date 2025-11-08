a8bc8ec6735643989235a2f21cf82790e7dfbf8a
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-11-08 12:58:31 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (65.00%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,13 +1,33 @@
 class Solution:
-    def uniquePaths(self, m: int, n: int) -> int:
-        @lru_cache(None)
-        def f(r,c):
-            if r < 1 or c < 1:
-                return 0
+    def uniquePaths(self, R: int, C: int) -> int:
+        """returns # of ways to reach to the bottom right corner"""
+        dp = [[0] * C for _ in range(R)]
 
-            if r==1 and c==1:
-                return 1
+        for col in range(C): dp[0][col] = 1
+        for row in range(R): dp[row][0] = 1
 
-            return f(r-1,c)+f(r,c-1)
+        for row in range(1, R): 
+            for col in range(1, C): 
+                dp[row][col] = dp[row-1][col] + dp[row][col-1]
 
-        return f(m,n)
+        return dp[R-1][C-1]
+    
+    def uniq_paths(R:int, C:int) -> int:
+        """returns # of ways to reach to the bottom right corner"""
+        dp = {}
+        def fn(r:int, c:int) -> int:
+            nonlocal dp
+
+            if not(0 <= r < R and 0 <= c < C): return 0
+
+            if r==R-1 and c==C-1: return 1
+            if (r,c) in dp: return dp[(r,c)]
+            op1 = fn(r,c+1)
+            op2 = fn(r+1,c)
+
+            dp[(r,c)] = op1+op2
+            return dp[(r,c)]
+
+        fn(0,0)
+
+        return dp[(0,0)]
