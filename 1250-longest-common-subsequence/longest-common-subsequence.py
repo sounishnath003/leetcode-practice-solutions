5f20512462a673ad9898ae62723686589108bcf9
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-11-08 15:08:14 +0530

    Time: 469 ms (55.97%) | Memory: 51.2 MB (21.97%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,28 +1,31 @@
-
-def lcs(s, t, si, ti, memo=None):
-    if memo is None:
-        memo = {}
-    
-    # Base cases
-    if si < 0 or ti < 0:
-        return 0
-    
-    # Check memo
-    if (si, ti) in memo:
-        return memo[(si, ti)]
-    
-    # If characters match
-    if s[si] == t[ti]:
-        memo[(si, ti)] = 1 + lcs(s, t, si-1, ti-1, memo)
-        return memo[(si, ti)]
-    
-    # If characters don't match, take maximum of both possibilities
-    memo[(si, ti)] = max(
-        lcs(s, t, si-1, ti, memo),
-        lcs(s, t, si, ti-1, memo)
-    )
-    return memo[(si, ti)]
-
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        return  lcs(text1,text2, len(text1)-1, len(text2)-1)
+        n=len(text1)
+        m=len(text2)
+
+        dp = {}
+
+        def f(n,m) -> int:
+            nonlocal dp, text1, text2
+            if n == 0 or m == 0: return 0
+            if (n,m) in dp: return dp[(n,m)]
+            # two options
+            if text1[n-1]==text2[m-1]:
+                dp[(n,m)] = 1 + f(n-1,m-1)
+                return dp[(n,m)]
+
+            dp[(n,m)] = max(f(n-1,m), f(n,m-1))
+            return dp[(n,m)]
+
+        # return f(n, m)
+
+        dp = [[0] * (m+1) for _ in range(n+1)]
+
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                if text1[i-1]==text2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+        return dp[n][m]
