b9b33463813e755501604c57b7b8b45e610f962e
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-12-14 12:42:47 +0530

    Time: 2985 ms (18.28%) | Memory: 18.1 MB (78.75%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.py
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.py
@@ -1,5 +1,6 @@
 class Solution:
     def minCut(self, s: str) -> int:
+        # LC: 132
         def palindrome_parition_min_cuts(s) -> int:
             def is_palindrome(s: str) -> bool: return s == s[::-1]
 
@@ -18,6 +19,19 @@ class Solution:
                 dp[i] = ans
                 return ans
 
-            return parition(s, 0, dp={}) 
+            # return parition(s, 0, dp={}) 
+
+            n = len(s)
+            dp = [float("inf")] * (n + 1)
+            dp[n] = -1
+
+            # other case:
+            for i in range(n-1, -1, -1):
+                for j in range(i, n):
+                    if is_palindrome(s[i:j+1]):
+                        cuts = 1 + dp[j+1]
+                        dp[i] = min(dp[i], cuts)
+
+            return dp[0]
 
         return palindrome_parition_min_cuts(s)
