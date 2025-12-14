e8baa46ac192c23f8f736e8e239836c174a988bf
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-12-14 15:41:40 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (15.59%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.py
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -1,24 +1,23 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        nodes = len(graph)
-        colors = [-1] * nodes  # -1 means uncolored
+        N = len(graph)
+        colors = [-1] * N
 
-        for node in range(nodes):
+        # bipartite says -> NO adjacent nodes must have same color
+        def dfs(node:int, color:int) -> bool:
+            """returns True or False, True on bipartite"""
+            colors[node] = color
+            # explore all neighbours
+            for neigh in graph[node]:
+                if colors[neigh] == -1: # uncolor and unvisited
+                    if not dfs(neigh, 1 - color): return False
+                elif (colors[neigh] == color): return False
+
+            return True
+
+        for node in range(N):
             if colors[node] == -1:
-                if not self.dfs(graph, node, colors, 0):
-                    return False
-
-        return True
-
-    def dfs(self, graph, node, colors, color):
-        if colors[node] != -1:
-            return colors[node] == color  # Check if consistent with previous color
-
-        colors[node] = color
-
-        for neighbor in graph[node]:
-            ctou = 1 if color == 0 else 0
-            if not self.dfs(graph, neighbor, colors, ctou):
-                return False
+                is_colorable = dfs(node, 0)
+                if not is_colorable: return False
 
         return True
