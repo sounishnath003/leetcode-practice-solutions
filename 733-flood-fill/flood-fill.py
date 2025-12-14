10035c2cac5e758705795ad4f5e22073b7b7ea98
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-12-14 21:00:31 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (13.56%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,38 +1,24 @@
-
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        
         R = len(image)
         C = len(image[0])
-        
-        # Correctly initialize the visited matrix using a list comprehension
-        visited = [[False for _ in range(C)] for _ in range(R)]
-        
-        start_color = image[sr][sc]
 
-        # Handle the case where the starting color is already the new color
-        if start_color == color:
-            return image
+        base_color = image[sr][sc] 
 
-        self.color_graph(image, sr, sc, R, C, start_color, color, visited)
-        return image
-        
-    def color_graph(self, image, row, col, R, C, start_color, color, visited):
-        visited[row][col] = True
-        image[row][col] = color
-        
-        dirs = [[-1,0], [0,1], [1,0], [0, -1]]
-        
-        for x, y in dirs:
-            nrow = row + x
-            ncol = col + y
-            
-            if nrow < 0 or nrow >= R or ncol < 0 or ncol >= C or visited[nrow][ncol]:
-                continue
+        visited = set()
+        if base_color == color: return image
 
-            if image[nrow][ncol] == start_color:
-                self.color_graph(image, nrow, ncol, R, C, start_color, color, visited)
-        
-        
-        # image[row][col] = 1
-        # visited[row][col] = False
+        def dfs(row: int, col: int, R:int, C:int, base_color:int, color:int):
+            visited.add((row, col))
+            image[row][col] = color
+
+            # explore neighbors
+            for dr, dc in [(0,-1),(-1,0),(0,1),(1,0)]:
+                nr = row+dr
+                nc = col+dc
+                # safe area check?
+                if not (0 <= nr < R and 0 <= nc < C) or image[nr][nc] != base_color or (nr, nc) in visited: continue
+                dfs(nr, nc, R, C, base_color, color)
+
+        dfs(sr, sc, R, C, base_color, color)
+        return image 
