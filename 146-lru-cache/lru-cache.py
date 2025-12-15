cbac172f3ff48b327a230c273815c3e74ea57da0
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-12-15 19:20:44 +0530

    Time: 133 ms (61.66%) | Memory: 78 MB (70.79%) - LeetSync

---
+++ 146-lru-cache/lru-cache.py
@@ -0,0 +1,57 @@
+class ListNode:
+    def __init__(self, key=0, val=0):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+
+        self.head = ListNode()
+        self.tail = ListNode()
+
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
+
+    def _add_to_front(self, node):
+        nxt = self.head.next
+        self.head.next = node
+        node.prev = self.head
+        node.next = nxt
+        nxt.prev = node
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+
+        node = self.cache[key]
+        self._remove(node)
+        self._add_to_front(node)
+        return node.val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            node = self.cache[key]
+            node.val = value
+            self._remove(node)
+            self._add_to_front(node)
+            return
+
+        if len(self.cache) == self.capacity:
+            lru = self.tail.prev
+            self._remove(lru)
+            del self.cache[lru.key]
+
+        node = ListNode(key, value)
+        self.cache[key] = node
+        self._add_to_front(node)
