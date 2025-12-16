d1d847938efdaef4d7b465655876111404cf30e8
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2025-12-17 01:07:29 +0530

    Time: 0 ms (100.00%) | Memory: 24.9 MB (45.92%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,4 +1,24 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        from functools import reduce
-        return reduce(lambda a,b: a^b, nums, 0)
+        # from functools import reduce
+        # return reduce(lambda a,b: a^b, nums, 0)
+        low=0
+        high=len(nums)-1
+
+        if len(nums)==1: return nums[0]
+        if nums[0]!=nums[1]: return nums[0]
+        if nums[-1]!=nums[-2]: return nums[-1]
+
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]!=nums[mid-1] and nums[mid]!=nums[mid+1]: return nums[mid]
+            elif nums[mid]==nums[mid-1]:
+                leftcount=(mid-low+1)
+                if leftcount%2==0: low=mid+1
+                else: high=mid-2
+            else:
+                rightcount=(high-mid+1)
+                if rightcount%2==0: high=mid-1
+                else: low=mid+2
+
+        return nums[low]
