23a6b7157f88225bd53456abfc5d0f4c493ba0b3
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2026-01-22 16:49:58 +0530

    Time: 4 ms (64.94%) | Memory: 20.6 MB (20.53%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -8,6 +8,32 @@ class Solution:
             return graph
 
         graph = build_graph(prerequisites)
+
+        # Using Kahns algorithm - BFS
+        def kahns(graph):
+            indegrees = [0] * numCourses
+            for u in graph:
+                for v in graph[u]:
+                    indegrees[v] += 1
+            
+            count = 0
+            q = deque()
+            for u in range(numCourses):
+                if indegrees[u]==0:
+                    q.append(u)
+                    count += 1
+
+            while q:
+                node = q.popleft()
+                for nei in graph[node]:
+                    indegrees[nei] -= 1
+                    if indegrees[nei] == 0:
+                        q.append(nei)
+                        count += 1
+            
+            return count == numCourses
+        
+        return kahns(graph)
         
         visited: Dict[int, str] = {node: "NOT_VISITED" for node in range(numCourses)}
         
