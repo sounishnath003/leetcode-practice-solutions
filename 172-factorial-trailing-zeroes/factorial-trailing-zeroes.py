f67017297f67dca453c49efc764b9fb8f26d1101
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2026-01-26 01:53:42 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (27.70%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.py
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.py
@@ -1,6 +1,7 @@
 class Solution:
     def trailingZeroes(self, n: int) -> int:
         # any number to be a 0 it must be divisable my 5 and its 5x
+        # TOO MUCH CODE
         cnt = 0
         five = 5
         while five <= n:
@@ -9,4 +10,8 @@ class Solution:
                 break
             five = five * 5
 		
-        return cnt 
+        cnt = 0
+        while n > 4:
+            cnt += int(n / 5)
+            n = n // 5
+        return cnt
