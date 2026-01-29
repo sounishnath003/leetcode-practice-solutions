4f207d100ad666aecabc33f18be42ead70de4537
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2026-01-29 15:44:28 +0530

    Time: 1120 ms (41.78%) | Memory: 21 MB (28.27%) - LeetSync

--- 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.py
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.py
@@ -32,16 +32,42 @@ class Solution:
                         heapq.heappush(pq, (nd, v))
             return dist
         
-        memo = {}
-        total_cost = 0
+        # memo = {}
+        # total_cost = 0
         
-        for s, t in zip(source, target):
-            if s == t:
-                continue
-            if s not in memo:
-                memo[s] = dijkstra(s)
-            if memo[s][t] == float('inf'):
-                return -1
-            total_cost += memo[s][t]
+        # for s, t in zip(source, target):
+        #     if s == t:
+        #         continue
+        #     if s not in memo:
+        #         memo[s] = dijkstra(s)
+        #     if memo[s][t] == float('inf'):
+        #         return -1
+        #     total_cost += memo[s][t]
         
-        return total_cost
+        # return total_cost
+
+        # Floyd warshall algorithm
+        A=26
+        INF = 10 ** 20
+        dist = [[INF] * A for _ in range(A) ]
+        for i in range(A):
+            dist[i][i] = 0
+        
+        for sc, tc, c in zip(original, changed, cost):
+            s = ord(sc) - ord('a')
+            t = ord(tc) - ord('a')
+            dist[s][t] = min(dist[s][t], c)
+
+
+        for k in range(A):
+            for u in range(A):
+                for v in range(A):
+                    dist[u][v] = min(dist[u][v], dist[u][k] + dist[k][v])
+
+        min_cost = 0
+        for sc, tc in zip(source, target):
+            s = ord(sc) - ord('a')
+            t = ord(tc) - ord('a')
+            min_cost += dist[s][t]
+
+        return -1 if min_cost >= INF else min_cost
