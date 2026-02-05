1881f71241b691e4a98641cc9c1aa1c7bbaf3ff3
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2026-02-05 15:50:18 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (6.63%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,29 +1,29 @@
 class Solution:
-    def findPeakElement(self, arr: List[int]) -> int:
-        # get the length of the arr
-        n=len(arr)
+    def findPeakElement(self, nums: list[int]) -> int:
+        n = len(nums)
         if n == 1:
             return 0
-        # base condition checks 
-        # if the 0th or n-1th element is the peak
-        if arr[0] > arr[1]:
+        
+        if nums[0] > nums[1]:
             return 0
-        if n-2 >= 0 and arr[n-1] > arr[n-2]:
+        if n-2 >= 0 and nums[n-1] > nums[n-2]:
             return n-1
-        # now we can apply binary search?
-        # through the array check if an element is greater than its neighbors
-        low=1
-        high=n-2
-        peak = -1
+        
+        low, high = 1, n-2
+        def is_peak(idx):
+            return (
+                idx > 0 and idx < n-1 
+                and nums[idx] > nums[idx-1] 
+                and nums[idx] > nums[idx+1]
+            )
+        
         while low <= high:
-            mid = (low + (high-low)//2)
-            # check if the mid element is the peak?
-            if arr[mid-1] < arr[mid] and arr[mid] > arr[mid+1]:
-                peak=mid
-                return peak
-            elif arr[mid] > arr[mid-1]:
-                low=mid+1
+            mid = (low + high) // 2
+            if nums[mid] > nums[mid-1] and nums[mid] > nums[mid+1]:
+                return mid
+            if nums[mid] > nums[mid-1]:
+                low = mid + 1
             else:
-                high=mid-1
-
-        return peak
+                high = mid - 1
+                
+        return -1
