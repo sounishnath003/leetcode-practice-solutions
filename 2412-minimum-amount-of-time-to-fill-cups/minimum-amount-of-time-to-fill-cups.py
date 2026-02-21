65879feb2c688a575f014a7038dcc1f1ce4381b9
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2026-02-22 02:13:56 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (30.97%) - LeetSync

--- 2412-minimum-amount-of-time-to-fill-cups/minimum-amount-of-time-to-fill-cups.py
+++ 2412-minimum-amount-of-time-to-fill-cups/minimum-amount-of-time-to-fill-cups.py
@@ -1,5 +1,5 @@
 class Solution:
     def fillCups(self, amount: List[int]) -> int:
-        op1 = max(amount)
-        op2 = math.ceil( sum(amount) / 2 )
+        op1 = max(amount) # aktai cup borte parbi jodi baki kono input na thake
+        op2 = math.ceil( sum(amount) / 2 ) # Oikick niyom
         return max(op1,  op2)
