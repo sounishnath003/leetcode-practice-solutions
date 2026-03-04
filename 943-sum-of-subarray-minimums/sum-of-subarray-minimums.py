e2c3a3fc0870f94d1c843a48781805d728cdab5b
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2026-03-04 19:12:56 +0530

    Time: 115 ms (32.78%) | Memory: 24.1 MB (50.02%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.py
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.py
@@ -1,34 +1,57 @@
+# LC: 907
+# https://leetcode.com/problems/sum-of-subarray-minimums/description/
 class Solution:
-    def sumSubarrayMins(self, arr: List[int]) -> int:
-        # define the MOD
+    def sumSubarrayMins(self, nums: list[int]) -> int:
+        """
+        1. Naive: O(N^2) ; SC: O(1)
+            1. you can generate all subarrays - 
+            2. check the minimum for all the subarrays
+            3. sum them up
+            4. return the total sum as result / answer
+
+        2. Optimized: TC: O(N + N + N) = O(3N) ; SC: O(2N)
+            1. Use of monotonic stack - means, keep the smaller elements only on the stack
+            2. for any element, 
+                2.1 check the next smaller elements (store the index)
+                2.2 check the smaller previous smaller equal elements (store the index)
+            3. simply, know the count the subarrays you can form using (R - L + 1) formula for that element.
+            4. total += local_sum
+            5. return the answer
+        """
+
+        def get_nses():
+            stack = []
+            nses = [len(nums)] * len(nums)
+            for i in range(len(nums) - 1, -1, -1):
+                while stack and nums[stack[-1]] > nums[i]: stack.pop()
+                nses[i] = len(nums) if not stack else stack[-1]
+                stack.append(i)
+
+            return nses
+
+        def get_psees():
+            stack = []
+            pses = [-1] * len(nums)
+            for i in range(len(nums)):
+                while stack and nums[stack[-1]] >= nums[i]: stack.pop()
+                pses[i] = -1 if not stack else stack[-1]
+                stack.append(i)
+
+            return pses
+
         MOD = int(1e9+7)
-        # get the length of the arr
-        n=len(arr)
-        # define the stack
-        stack=[] # (index, curr_elem)
-        # resultant sum of minimums
-        result=0
-        # iterate over the array
+        n = len(nums)
+        nse = get_nses()
+        psee = get_psees()
+
+        total = 0
         for i in range(n):
-            curr_elem=arr[i]
-            # keep the minimum elements into the stack always
-            while stack and curr_elem < stack[-1][1]:
-                # pop the elem
-                j,m=stack.pop()
-                # so we have popped the i th elements
-                # 1 2 3 4 5 1
-                #.    j    i
-                total_elements_in_right=i-j
-                total_elements_in_left=j - stack[-1][0] if stack else j+1
-                result = (result + (m * total_elements_in_left * total_elements_in_right)%MOD) % MOD
-            # put the elemt into the stack
-            stack.append((i,curr_elem))
+            left_boundary, right_boundary = i - psee[i], nse[i] - i
+            # left and right boundary provides the indexes .
+            # to # subarray just substract from curr index
+            # wrong formula: local_sum = (nums[i] * (left_boundary + right_boundary)) % MOD
+            # we need to know, how many combination of subarray be formed with L and R counts
+            local_sum = (left_boundary * right_boundary * nums[i]) % MOD
+            total = (total + local_sum) % MOD
 
-        for i in range(len(stack)):
-            j,m=stack[i]
-            left=j-stack[i-1][0] if i>0 else j+1
-            right=len(arr)-j
-
-            result=(result+(m*left*right)%MOD) % MOD
-
-        return result
+        return total
