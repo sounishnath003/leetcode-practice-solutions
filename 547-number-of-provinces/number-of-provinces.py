46896e6ccf51d0256c118ebfdd9dbb172e41f41f
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2026-03-15 01:43:10 +0530

    Time: 4 ms (68.33%) | Memory: 21.5 MB (33.53%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,48 +1,55 @@
 # https://leetcode.com/problems/number-of-provinces/
 
-from collections import defaultdict
-from typing import List
-
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        """looks like we need to find the strongly connected components"""
-        # get the length
-        R=len(isConnected)
-        C=len(isConnected[0])
-        # total number of nodes
-        NODES=R
-        """to find the strongly-connected-components; 1. DFS(2-way kosaraju dfs)"""
-        # create the adjancent list
-        graph = defaultdict(list)
-        for u in range(R):
-            for v in range(C):
-                if isConnected[u][v] == 1:
-                    graph[u].append(v)
-                    
-        """find the DFS function"""
-        def dfs1(graph,node,visited,topolist):
-            # mark as visited the node
-            visited.add(node)
-            # explore all the nbr and perform dfs
-            for nbr in graph[node]:
-                if nbr not in visited:
-                    dfs1(graph,nbr,visited,topolist)
-            # add the node into the topological ordering
-            topolist.append(node)
-        
-        topolist=[]
-        visited=set()
-        scc=0
-        for node in range(NODES):
-            if node not in visited:
-                dfs1(graph,node,visited,topolist)
-                # increment the ssc as what all the nodes were visited
-                scc +=1
+        n = len(isConnected)
+        visited = set()
+        provinces = 0
 
-        # # ones the topological ordering is completed, 
-        # # we need to count strong components using the dfs2 and reversing the links
-        # # create a graph 2
-        # graph2=defaultdict(list)
-        # for u in range(R):
-        #     for v in range(C):
-        return scc
+        def dfs(node):
+            visited.add(node)
+            # Check all possible neighbors
+            for neighbor in range(n):
+                # If connected AND not visited yet
+                if isConnected[node][neighbor] == 1 and neighbor not in visited:
+                    dfs(neighbor)
+
+        for i in range(n):
+            if i not in visited:
+                # We found a new "island", start a DFS to mark the whole island
+                dfs(i)
+                # Increment the count once per island found
+                provinces += 1
+                
+        return provinces
+
+
+class Solution2:
+    def findCircleNum(self, isConnected: list[list[int]]) -> int:
+        n = len(isConnected)
+        parent = list(range(n))
+        # Start with n provinces, we will subtract as we merge
+        self.provinces = n 
+        
+        def find(i):
+            if parent[i] == i:
+                return i
+            # Path Compression: makes future finds O(1)
+            parent[i] = find(parent[i]) 
+            return parent[i]
+            
+        def union(i, j):
+            root_i = find(i)
+            root_j = find(j)
+            if root_i != root_j:
+                parent[root_i] = root_j
+                # Two separate groups just became one
+                self.provinces -= 1
+        
+        # Check every pair of cities
+        for i in range(n):
+            for j in range(i + 1, n):
+                if isConnected[i][j] == 1:
+                    union(i, j)
+                    
+        return self.provinces
