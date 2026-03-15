526bcaa039ce8c53675e1f8b7dcf424e8d83f1e1
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2026-03-15 11:20:14 +0530

    Time: 27 ms (89.88%) | Memory: 20.5 MB (94.61%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,24 +1,16 @@
-
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        # store indices of invalid parentheses in stack
-        stack = [] # stores indices of opening brackets
         s_list = list(s)
-        # First pass - mark invalid closing brackets
-        for i in range(len(s)):
-            if s[i] == '(':
-                # Store index of opening bracket
-                stack.append(i)
-            elif s[i] == ')':
-                # For closing bracket, check if we have matching opening bracket
-                if stack and s[stack[-1]] == '(':
-                    stack.pop()
-                else:
-                    # No matching opening bracket, mark as invalid
+        stack = []
+
+        for i, char in enumerate(s_list):
+            if char == '(': stack.append(i)
+            elif char == ')':
+                if not stack or s_list[stack[-1]] != '(':
                     s_list[i] = ''
-        # Second pass - remove unmatched opening brackets
-        while stack:
-            s_list[stack.pop()] = ''
-            
-        # Join remaining characters and return result
-        return ''.join(s_list)
+                else:
+                    stack.pop()
+
+        while stack: s_list[stack.pop()] = ''
+
+        return "".join(s_list)
