299555533e763b1d7325566fa446a1a20d975ff7
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2026-03-22 01:24:45 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (76.66%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.py
+++ 101-symmetric-tree/symmetric-tree.py
@@ -1,15 +1,12 @@
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        def is_symmetric_tree(root1, root2):
-            """my subtrees left and right values and tree must be valid"""
+        def fn(root1: TreeNode, root2: TreeNode) -> bool:
             if not root1 and not root2:
                 return True
-            if not root1 or not root2:
-                return False
-            return (
-                root1.val == root2.val
-                and is_symmetric_tree(root1.left, root2.right)
-                and is_symmetric_tree(root1.right, root2.left)
-            )
 
-        return is_symmetric_tree(root, root)
+            if (not root1 and root2) or (root1 and not root2 or root1.val != root2.val):
+                return False
+
+            return fn(root1.left, root2.right) and fn(root1.right, root2.left)
+
+        return fn(root, root)
