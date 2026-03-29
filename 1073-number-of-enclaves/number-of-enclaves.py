431ee95d9a9ff51afaf60fd0e8af46bfb0a94013
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2026-03-29 22:51:57 +0530

    Time: 118 ms (26.08%) | Memory: 42.5 MB (21.50%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.py
@@ -0,0 +1,36 @@
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        R, C = len(grid), len(grid[0])
+
+        def is_boundary(r:int, c:int) -> int:
+            return (r == 0 or r == R-1 or c == 0 or c == C-1)
+        
+        def safe(r:int, c:int) -> int:
+            return (0 <= r < R and 0 <= c < C)
+
+        def dfs(row:int, col:int):
+            grid[row][col] = 2
+
+            dirs = [(-1,0),(0,1),(1,0),(0,-1)]
+            for dr,dc in dirs:
+                nrow,ncol = row+dr, col+dc
+                
+                if safe(nrow,ncol) and grid[nrow][ncol] == 1:
+                    dfs(nrow,ncol)
+
+
+        # Get all the boundary 1s and DFS and mark them 2. (checking how far we can reach?)
+        for row in range(R):
+            for col in range(C):
+                if is_boundary(row,col) and grid[row][col] == 1:
+                    dfs(row,col)
+
+        # Simply count the left over 1s
+        enclaves = 0
+        for row in range(R):
+            for col in range(C):
+                if grid[row][col] == 1:
+                    enclaves += 1
+
+
+        return enclaves
